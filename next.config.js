c44fb62c5be98d8a7aa3b3df31426b5ec12395a1
tszhong0411 <lainelson411@gmail.com>
2022-02-01 04:42:00 +0800

    修復 script 問題

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app cdn.jsdelivr.net www.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app cdn.jsdelivr.net www.googletagmanager.com www.google-analytics.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net *.fontawesome.com;
   img-src * blob: data:;
   media-src cdn.jsdelivr.net;
