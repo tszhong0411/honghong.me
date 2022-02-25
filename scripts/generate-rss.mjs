09f594b9b0130a35ab7e5abcc3bcf290453803d6
tszhong0411 <lainelson411@gmail.com>
2022-02-25 18:45:49 +0800

    DEV

---
+++ scripts/generate-rss.mjs
@@ -0,0 +1,53 @@
+import { writeFileSync } from "fs";
+import { escape } from "./htmlEscaper.mjs";
+import siteMetadata from "../data/siteMetadata.js";
+import { allBlogs } from "../.contentlayer/generated/index.mjs";
+import i18n from "../i18n.json";
+
+const generateRssItem = (post) => {
+  return `
+  <item>
+    <guid>${siteMetadata.siteUrl}/blog/${post.slug}</guid>
+    <title>${escape(post.title)}</title>
+    <link>${siteMetadata.siteUrl}/blog/${post.slug}</link>
+    ${post.summary && `<description>${escape(post.summary)}</description>`}
+    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    <author>${siteMetadata.email} (${siteMetadata.author})</author>
+  </item>
+`;
+};
+
+const generateRss = (posts, page = "feed.xml", lang) => {
+  return `
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+      <title>${escape(siteMetadata.title)}</title>
+      <link>${siteMetadata.siteUrl}/blog</link>
+      <description>${escape(siteMetadata.description[lang])}</description>
+      <language>${lang}</language>
+      <managingEditor>${siteMetadata.email} (${siteMetadata.author})</managingEditor>
+      <webMaster>${siteMetadata.email} (${siteMetadata.author})</webMaster>
+      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <atom:link href="${siteMetadata.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
+      ${posts.map(generateRssItem).join("")}
+    </channel>
+  </rss>
+`;
+};
+
+async function generate() {
+  // RSS for blog post
+  if (allBlogs.length > 0) {
+    // for each lang
+    i18n.locales.forEach((lang) => {
+      var rss = generateRss(
+        allBlogs.filter((post) => post.slug.split("/")[0] === lang),
+        `feed${lang !== i18n.defaultLocale ? `.${lang}` : ""}.xml`,
+        lang,
+      );
+      writeFileSync(`./public/feed${lang !== i18n.defaultLocale ? `.${lang}` : ""}.xml`, rss);
+    });
+  }
+}
+
+generate();
---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,51 @@
+import { writeFileSync } from "fs";
+import globby from "globby";
+import prettier from "prettier";
+import siteMetadata from "../data/siteMetadata.js";
+import { allBlogs } from "../.contentlayer/generated/index.mjs";
+
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
+  const contentPages = allBlogs
+    .map((x) => `/${x._raw.flattenedPath}`)
+    .filter((x) => !x.draft && !x.canonicalUrl);
+  const pages = await globby([
+    "pages/*.{js|tsx}",
+    "!pages/_*.{js|tsx}",
+    "!pages/api",
+    "!pages/404.{js|tsx}",
+  ]);
+
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+              .concat(contentPages)
+              .map((page) => {
+                const path = page
+                  .replace("pages/", "/")
+                  .replace("public/", "/")
+                  .replace(".js", "")
+                  .replace(".mdx", "")
+                  .replace(".md", "")
+                  .replace("/feed.xml", "");
+                const route = path === "/index" ? "" : path;
+                return `
+                        <url>
+                            <loc>${siteMetadata.siteUrl}${route}</loc>
+                        </url>
+                    `;
+              })
+              .join("")}
+        </urlset>
+    `;
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: "html",
+  });
+
+  writeFileSync("public/sitemap.xml", formatted);
+}
+
+generate();
