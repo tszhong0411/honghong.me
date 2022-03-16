d07f3cd60ab30e3f73b56abba81f025cfb11be2b
小康 <lainelson411@gmail.com>
2022-03-16 13:31:58 +0800

    test(mdxcomponents): try remove alert import

--- components/MDXComponents/MDXComponents.tsx
+++ components/MDXComponents/MDXComponents.tsx
@@ -1,13 +1,14 @@
-import Alert from '../Alert'
-import { CloudinaryImg } from '../Image'
-import CustomLink from '../Link'
-import Pre from '../Pre'
+// import { Alert } from '@/components/Alert'
+import { CloudinaryImg } from '@/components/Image'
+import CustomLink from '@/components/Link'
+import Pre from '@/components/Pre'
+
 import { YouTubeEmbed } from '../YouTubeEmbed'
 
 export const MDXComponents = {
   CloudinaryImg,
   YouTubeEmbed,
-  Alert,
+  // Alert,
   a: CustomLink,
   pre: Pre,
 }
--- components/alert/index.tsx
+++ components/alert/index.tsx
@@ -1,1 +1,1 @@
-export { default } from './Alert'
+export { Alert } from './Alert'
