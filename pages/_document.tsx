914772ea278ed36fea246567b18470dffae6787b
小康 <lainelson411@gmail.com>
2022-05-06 09:12:53 +0800

    refactor: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,46 @@ class MyDocument extends Document {
           />
           <link
             rel="apple-touch-icon"
+            sizes="57x57"
+            href="/static/favicons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/static/favicons/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/static/favicons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/favicons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/static/favicons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/static/favicons/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/static/favicons/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/favicons/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
             sizes="180x180"
             href="/static/favicons/apple-touch-icon.png"
           />
@@ -28,7 +68,7 @@ class MyDocument extends Document {
             href="/static/favicons/favicon-16x16.png"
           />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#373737" />
-          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico?v=2" />
           <meta name="apple-mobile-web-app-title" content="小康" />
           <meta name="application-name" content="小康" />
           <meta name="msapplication-TileColor" content="#ffffff" />
