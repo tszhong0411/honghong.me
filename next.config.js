00928b81b6faf20b8f9b0cd464b1c41324987c2a
小康 <lainelson411@gmail.com>
2022-05-29 15:51:42 +0800

    refactor: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from 'next-contentlayer'
+const { withContentlayer } = require('next-contentlayer')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
