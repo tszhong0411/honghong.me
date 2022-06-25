a1fd4060050cb4e19517b489f532ea64e749d406
小康 <lainelson411@gmail.com>
2022-06-25 15:50:42 +0800

    :sparkles: Save the user-defined language
    Detect language and redirect the router if 'locale' isn't in 'localstorage', else redirect to the language selected by the user.

--- src/components/LanguageSwitch/LanguageSwitch.tsx
+++ src/components/LanguageSwitch/LanguageSwitch.tsx
@@ -1,24 +1,37 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { FaGlobe } from 'react-icons/fa';
+import { useLocalStorage } from 'react-use';
 
 import { Tooltip } from '@/components/Tooltip';
+
+import i18n from '../../../i18n.json';
 
 export default function LanguageSwitch({ open, setOpen }) {
   const router = useRouter();
   const { t } = useTranslation();
+  const [locale, setLocale] = useLocalStorage('locale');
+
+  // Redirect router when locale not set in 'localstorage'
+  React.useEffect(() => {
+    if (locale === undefined) {
+      router.push(router.asPath, router.asPath, {
+        locale: navigator.languages.find((config) =>
+          i18n.locales.some((current) => current === config)
+        ),
+      });
+    } else if (typeof locale === 'string') {
+      locale !== i18n.defaultLocale &&
+        router.push(router.asPath, router.asPath, { locale });
+    }
+  }, [locale, router]);
 
   const changeLanguage = (locale: string) => {
+    setLocale(locale);
     router.push(router.asPath, router.asPath, { locale });
   };
-
-  // https://github.com/framer/motion/issues/578
-  const [isLoaded, setLoaded] = useState(false);
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
 
   return (
     <>
@@ -41,39 +54,37 @@ export default function LanguageSwitch({ open, setOpen }) {
           <FaGlobe size={20} />
         </motion.button>
       </Tooltip>
-      {isLoaded && (
-        <AnimatePresence>
-          {open && (
-            <motion.div
-              animate={{ y: 0 }}
-              initial={{ y: -200 }}
-              exit={{ y: -200, opacity: 0 }}
-              className='fixed top-[60px] right-0 z-50 flex flex-row gap-x-4 rounded-md border-2 border-slate-300 bg-body-secondary py-2 px-4 dark:border-border-primary-dark dark:bg-body-secondary-dark md:absolute'
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            animate={{ y: 0 }}
+            initial={{ y: -200 }}
+            exit={{ y: -200, opacity: 0 }}
+            className='fixed top-[60px] right-0 z-50 flex flex-row gap-x-4 rounded-md border-2 border-slate-300 bg-body-secondary py-2 px-4 dark:border-border-primary-dark dark:bg-body-secondary-dark md:absolute'
+          >
+            <div
+              className='cursor-pointer rounded-md px-4 py-2 duration-300 hover:bg-body dark:hover:bg-body-dark'
+              onClick={() => {
+                setOpen(false);
+                changeLanguage('zh-TW');
+              }}
+              aria-hidden='true'
             >
-              <div
-                className='cursor-pointer rounded-md px-4 py-2 duration-300 hover:bg-body dark:hover:bg-body-dark'
-                onClick={() => {
-                  setOpen(false);
-                  changeLanguage('zh-TW');
-                }}
-                aria-hidden='true'
-              >
-                繁體中文
-              </div>
-              <div
-                className='cursor-pointer rounded-md px-4 py-2 duration-300 hover:bg-body dark:hover:bg-body-dark'
-                onClick={() => {
-                  setOpen(false);
-                  changeLanguage('en');
-                }}
-                aria-hidden='true'
-              >
-                English
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      )}
+              繁體中文
+            </div>
+            <div
+              className='cursor-pointer rounded-md px-4 py-2 duration-300 hover:bg-body dark:hover:bg-body-dark'
+              onClick={() => {
+                setOpen(false);
+                changeLanguage('en');
+              }}
+              aria-hidden='true'
+            >
+              English
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
