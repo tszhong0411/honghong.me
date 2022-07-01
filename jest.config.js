ecc8eea517020ebe7c7e702c203cda02952b3bf4
小康 <lainelson411@gmail.com>
2022-07-01 14:20:50 +0800

    :wrench: [Jest] Wrong file extension of config file

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
