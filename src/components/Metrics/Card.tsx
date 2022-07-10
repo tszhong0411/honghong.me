a9e85b98170cd524b15e8bf2ae1d86bd43574157
小康 <lainelson411@gmail.com>
2022-07-10 17:42:45 +0800

    :bug: Fix Skeleton is not show

--- src/components/Metrics/Card.tsx
+++ src/components/Metrics/Card.tsx
@@ -1,4 +1,4 @@
-import { Card, Group, Skeleton, Text, useMantineTheme } from '@mantine/core';
+import { Card, Skeleton, Text, useMantineTheme } from '@mantine/core';
 import React from 'react';
 
 import Link from '@/components/Link';
@@ -8,37 +8,40 @@ export default function MetricCard({ header, link, metric, isCurrency }) {
   const dark = theme.colorScheme === 'dark';
 
   return (
-    <Card shadow='sm' p='lg' radius='lg'>
-      <Group direction='column'>
-        <Link
-          aria-label={header}
-          href={link}
-          sx={{
-            color: dark ? '#C1C2C5' : '#1f2937',
-            ...(dark && {
-              '&:hover': {
-                color: 'white',
-              },
-            }),
-          }}
-        >
-          {header}
-        </Link>
-        <Text
-          sx={{
-            fontSize: 36,
-            fontWeight: 800,
-            lineHeight: '40px',
-          }}
-        >
-          {metric > 0 && isCurrency && '$'}
-          {metric > 0 ? (
-            metric.toLocaleString()
-          ) : (
-            <Skeleton height={20} radius='xl' />
-          )}
-        </Text>
-      </Group>
+    <Card
+      shadow='sm'
+      p='lg'
+      radius='lg'
+      sx={{ display: 'flex', flexDirection: 'column', gap: 8 }}
+    >
+      <Link
+        aria-label={header}
+        href={link}
+        sx={{
+          color: dark ? '#C1C2C5' : '#1f2937',
+          ...(dark && {
+            '&:hover': {
+              color: 'white',
+            },
+          }),
+        }}
+      >
+        {header}
+      </Link>
+      <Text
+        sx={{
+          fontSize: 36,
+          fontWeight: 800,
+          lineHeight: '40px',
+        }}
+      >
+        {metric > 0 && isCurrency && '$'}
+        {metric > 0 ? (
+          metric.toLocaleString()
+        ) : (
+          <Skeleton height={40} radius='xl' width='35%' />
+        )}
+      </Text>
     </Card>
   );
 }
