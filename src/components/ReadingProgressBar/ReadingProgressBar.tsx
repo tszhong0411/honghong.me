d2055fbde94c0b3db49149d28327f50022634a06
tszhong0411 <lainelson411@gmail.com>
2022-08-08 23:02:23 +0800

    :recycle: Use Navigation progress to make reading progress bar

--- src/components/ReadingProgressBar/ReadingProgressBar.tsx
+++ src/components/ReadingProgressBar/ReadingProgressBar.tsx
@@ -1,18 +1,14 @@
+import { NavigationProgress, setNavigationProgress } from '@mantine/nprogress'
 import React from 'react'
 
-import { useStyles } from './ReadingProgressBar.styles'
-
 export default function ReadingProgressBar() {
-  const [width, setWidth] = React.useState(0)
-  const { classes } = useStyles()
-
   const scrollHeight = () => {
     const el = document.documentElement,
       ScrollTop = el.scrollTop || document.body.scrollTop,
       ScrollHeight = el.scrollHeight || document.body.scrollHeight
-    const percent = ScrollTop / (ScrollHeight - el.clientHeight)
+    const percent = (ScrollTop / (ScrollHeight - el.clientHeight)) * 100
 
-    setWidth(percent)
+    setNavigationProgress(percent)
   }
 
   React.useEffect(() => {
@@ -20,7 +16,5 @@ export default function ReadingProgressBar() {
     return () => window.removeEventListener('scroll', scrollHeight)
   })
 
-  return (
-    <div className={classes.wrapper} style={{ width: `${width * 100}%` }}></div>
-  )
+  return <NavigationProgress autoReset={false} transitionDuration={0} />
 }
