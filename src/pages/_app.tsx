af3c6a2382eb3b82dae7f19efcdb69438f303fa9
tszhong0411 <lainelson411@gmail.com>
2022-08-18 21:18:55 +0800

    :recycle: 用 local Storage 代替 cookie 儲存模式偏好設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,38 @@
 import {
   ColorScheme,
   ColorSchemeProvider,
-  Container,
   Global,
   MantineProvider,
 } from '@mantine/core'
-import { useHotkeys } from '@mantine/hooks'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
 import { ModalsProvider } from '@mantine/modals'
 import { NotificationsProvider } from '@mantine/notifications'
 import { SpotlightProvider } from '@mantine/spotlight'
-import { getCookie, setCookies } from 'cookies-next'
-import { GetServerSidePropsContext } from 'next'
+import { IconSearch } from '@tabler/icons'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { Search } from 'tabler-icons-react'
 
 import '@/styles/global.css'
 import '@/styles/prism.css'
 
 import { isProd } from '@/lib/isProduction'
 
-import Footer from '@/components/Layout/Footer'
-import Header from '@/components/Layout/Header'
 import { links } from '@/components/Layout/Header/links'
-import RouterTransition from '@/components/RouterTransition'
 import Umami from '@/components/Umami'
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props
-  const [colorScheme, setColorScheme] = React.useState<ColorScheme>(
-    props.colorScheme
-  )
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  })
   const router = useRouter()
 
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
-    setColorScheme(nextColorScheme)
-    setCookies('mantine-color-scheme', nextColorScheme, {
-      maxAge: 60 * 60 * 24 * 30,
-    })
-  }
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
 
   useHotkeys([['mod+J', () => toggleColorScheme()]])
 
@@ -60,96 +52,73 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   }
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme,
-          fontFamily:
-            'Sora,Noto Sans TC,Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
-          primaryColor: 'red',
-          breakpoints: {
-            xs: 375,
-            sm: 640,
-            md: 768,
-            lg: 1024,
-            xl: 1280,
-          },
-        }}
+    <SessionProvider>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <ModalsProvider>
-          <NotificationsProvider>
-            <SpotlightProvider
-              searchIcon={<Search size={18} />}
-              searchPlaceholder='Search'
-              shortcut={['mod + k', 'mod + p']}
-              nothingFoundMessage='Nothing found'
-              actions={Actions()}
-              radius='md'
-              highlightQuery
-            >
-              <RouterTransition />
-              <Global
-                styles={() => ({
-                  html: {
-                    scrollBehavior: 'smooth',
-                  },
-                  'ol, ul, menu': {
-                    listStyle: 'none',
-                    margin: 0,
-                    padding: 0,
-                  },
-                  '::selection': {
-                    background: 'rgb(249, 6, 6, 0.05)',
-                    color: '#f90606',
-                  },
-                  '::-webkit-scrollbar': {
-                    width: 7,
-                    height: 5,
-                  },
-                  '::-webkit-scrollbar-thumb': {
-                    background: '#ef4444',
-                    transition: '0.25s',
-                    borderRadius: 2,
-                  },
-                  '::-webkit-scrollbar-track': {
-                    background: '0 0',
-                  },
-                  // https://stackoverflow.com/questions/61083813/how-to-avoid-internal-autofill-selected-style-to-be-applied
-                  'input:-webkit-autofill, input:-webkit-autofill:focus': {
-                    transition: 'background-color 600000s 0s, color 600000s 0s',
-                  },
-                  // Blog youtube embed
-                  '.yt-lite': {
-                    margin: '32px 0',
-                  },
-                })}
-              />
-              {isProd && <Umami />}
-              <Header />
-              <Container
-                sx={(theme) => ({
-                  padding: '24px 16px',
-                  [theme.fn.largerThan('sm')]: {
-                    padding: '48px 32px',
-                  },
-                })}
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+            fontFamily:
+              'Sora,Noto Sans TC,Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+            primaryColor: 'red',
+            breakpoints: {
+              xs: 375,
+              sm: 640,
+              md: 768,
+              lg: 1024,
+              xl: 1280,
+            },
+          }}
+        >
+          <ModalsProvider>
+            <NotificationsProvider>
+              <SpotlightProvider
+                searchIcon={<IconSearch size={18} />}
+                searchPlaceholder='Search'
+                shortcut={['mod + k', 'mod + p']}
+                nothingFoundMessage='Nothing found'
+                actions={Actions()}
+                radius='md'
+                highlightQuery
               >
+                <Global
+                  styles={() => ({
+                    html: {
+                      scrollBehavior: 'smooth',
+                    },
+                    '::selection': {
+                      background: 'rgb(249, 6, 6, 0.05)',
+                      color: '#f90606',
+                    },
+                    '::-webkit-scrollbar': {
+                      width: 7,
+                      height: 5,
+                    },
+                    '::-webkit-scrollbar-thumb': {
+                      background: '#ef4444',
+                      transition: '0.25s',
+                      borderRadius: 2,
+                    },
+                    '::-webkit-scrollbar-track': {
+                      background: '0 0',
+                    },
+                    'input:-webkit-autofill, input:-webkit-autofill:focus': {
+                      transition:
+                        'background-color 600000s 0s, color 600000s 0s',
+                    },
+                  })}
+                />
+                {isProd && <Umami />}
                 <Component {...pageProps} />
-              </Container>
-              <Footer />
-            </SpotlightProvider>
-          </NotificationsProvider>
-        </ModalsProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+              </SpotlightProvider>
+            </NotificationsProvider>
+          </ModalsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </SessionProvider>
   )
 }
-
-App.getInitialProps = async ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
-})
