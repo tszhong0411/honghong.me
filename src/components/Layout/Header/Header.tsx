27109a672641785f8c255126738a0c30bd45a431
tszhong0411 <lainelson411@gmail.com>
2022-08-18 20:08:52 +0800

    :art: Sort the import

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -3,14 +3,13 @@ import { useDisclosure } from '@mantine/hooks'
 import { useRouter } from 'next/router'
 import React from 'react'
 
+import { useStyles } from '@/components/Layout/Header/Header.styles'
 import HeaderLogo from '@/components/Layout/Header/HeaderLogo'
 import LanguageSwitch from '@/components/Layout/Header/LanguageSwitch'
 import { links } from '@/components/Layout/Header/links'
 import Search from '@/components/Layout/Header/Search'
 import ThemeSwitch from '@/components/Layout/Header/ThemeSwitch'
 import Link from '@/components/Link'
-
-import { useStyles } from './Header.styles'
 
 export default function Header() {
   const { classes, cx } = useStyles()
