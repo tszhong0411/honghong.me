95d6570db36c4caab1b755281904d430205d89a3
tszhong0411 <lainelson411@gmail.com>
2022-08-18 20:01:43 +0800

    :lipstick: 移除浮動 label 效果

--- src/components/Guestbook/Guestbook.styles.ts
+++ src/components/Guestbook/Guestbook.styles.ts
@@ -1,62 +1,28 @@
 import { createStyles } from '@mantine/core'
 
-export const useStyles = createStyles(
-  (theme, { floating }: { floating: boolean }) => ({
-    root: {
-      position: 'relative',
-    },
+export const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+  },
 
-    label: {
-      position: 'absolute',
-      zIndex: 2,
-      top: 7,
-      left: theme.spacing.sm,
-      pointerEvents: 'none',
-      color: floating
-        ? theme.colorScheme === 'dark'
-          ? theme.white
-          : theme.black
-        : theme.colorScheme === 'dark'
-        ? theme.colors.dark[3]
-        : theme.colors.gray[5],
-      transition:
-        'transform 150ms ease, color 150ms ease, font-size 150ms ease',
-      transform: floating ? `translate(-${theme.spacing.sm}px, -28px)` : 'none',
-      fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
-      fontWeight: floating ? 500 : 400,
+  btn: {
+    width: '100%',
+    maxWidth: '100%',
+    [theme.fn.largerThan('sm')]: {
+      maxWidth: 100,
     },
+  },
 
-    required: {
-      transition: 'opacity 150ms ease',
-      opacity: floating ? 1 : 0,
+  formWrapper: {
+    flexDirection: 'column',
+    gap: 0,
+    [theme.fn.largerThan('sm')]: {
+      flexDirection: 'row',
+      gap: 16,
     },
+  },
 
-    input: {
-      '&::placeholder': {
-        transition: 'color 150ms ease',
-        color: !floating ? 'transparent' : undefined,
-      },
-    },
-
-    btn: {
-      width: '100%',
-      maxWidth: '100%',
-      [theme.fn.largerThan('sm')]: {
-        maxWidth: 100,
-      },
-    },
-
-    formWrapper: {
-      flexDirection: 'column',
-      gap: 0,
-      [theme.fn.largerThan('sm')]: {
-        flexDirection: 'row',
-        gap: 16,
-      },
-    },
-
-    avatar: {
-      borderRadius: '50%',
-    },
-  })
-)
+  avatar: {
+    borderRadius: '50%',
+  },
+}))
