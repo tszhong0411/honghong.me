a9671d3ec875e87d80e0407d9970545bc0e44b7a
tszhong0411 <lainelson411@gmail.com>
2022-08-19 01:35:41 +0800

    :bug: console log error

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -42,6 +42,7 @@ export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
       props: { post, ogImage },
     }
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error)
     return {
       notFound: true,
