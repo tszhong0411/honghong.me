fd42a78c2973a732a829c089015a9bf9b7fef944
tszhong0411 <lainelson411@gmail.com>
2022-08-22 23:24:52 +0800

    :lipstick: New button style

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,15 +1,12 @@
-import { Box, Button, Card, Title } from '@mantine/core'
-import { useHover } from '@mantine/hooks'
-import { IconArrowRight } from '@tabler/icons'
-import { motion } from 'framer-motion'
+import Link from '../Link'
+
+import { Button, Card, Title } from '@mantine/core'
 import useTranslation from 'next-translate/useTranslation'
 
-import Link from '@/components/Link'
 import { ProjectCardProps } from '@/components/ProjectCard/types'
 
 const ProjectsCard = ({ title, description, href }: ProjectCardProps) => {
   const { t } = useTranslation('common')
-  const { hovered, ref } = useHover()
 
   return (
     <Card shadow='sm' radius='lg' p={32}>
@@ -17,21 +14,18 @@ const ProjectsCard = ({ title, description, href }: ProjectCardProps) => {
         {title}
       </Title>
       <p>{description}</p>
-      <Box sx={{ display: 'inline-block' }} ref={ref}>
-        <Button
-          component={Link}
-          href={href}
-          rightIcon={
-            <motion.div animate={{ x: hovered ? 5 : 0 }}>
-              <IconArrowRight size={20} />
-            </motion.div>
-          }
-          noIcon
-          underline={false}
-        >
-          {t('visit')}
-        </Button>
-      </Box>
+      <Button
+        component={Link}
+        href={href}
+        variant='light'
+        fullWidth
+        mt='md'
+        radius='md'
+        noIcon
+        underline={false}
+      >
+        {t('visit')}
+      </Button>
     </Card>
   )
 }
