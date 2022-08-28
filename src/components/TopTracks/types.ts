43f177316d1e06ad053ae8f4d5d4c1a01ee33f63
tszhong0411 <lainelson411@gmail.com>
2022-08-28 18:02:13 +0800

    :fire: Remove top tracks

--- src/components/TopTracks/types.ts
+++
@@ -1,9 +0,0 @@
-export type Song = {
-  songUrl: string
-  artist: string
-  title: string
-}
-
-export type TopTracks = {
-  tracks: Song[]
-}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Title } from '@mantine/core'
+import { Box } from '@mantine/core'
 import useTranslation from 'next-translate/useTranslation'
 
 import { isProd } from '@/lib/isProduction'
@@ -8,7 +8,6 @@ import PageLayout from '@/components/Layout/PageLayout'
 import BlogTotalViews from '@/components/Metrics/BlogTotalViews'
 import Github from '@/components/Metrics/Github'
 import Youtube from '@/components/Metrics/Youtube'
-import TopTracks from '@/components/TopTracks'
 
 export default function Dashboard() {
   const { t } = useTranslation('common')
@@ -31,18 +30,6 @@ export default function Dashboard() {
             </>
           )}
         </Box>
-        <Title
-          order={2}
-          mb={16}
-          mt={64}
-          sx={{
-            fontSize: 30,
-            fontWeight: 700,
-          }}
-        >
-          {t('Dashboard_spotifyTitle')}
-        </Title>
-        <TopTracks />
       </PageLayout>
     </Layout>
   )
