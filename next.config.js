186c10a67036c33a97d1a85ca7a1560e00018288
tszhong0411 <lainelson411@gmail.com>
2022-10-30 00:36:48 +0800

    :fire: Remove unused code and dependency

--- next.config.js
+++ next.config.js
@@ -1,61 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 const nextTranslate = require('next-translate')
 
 /** @type {import('next').NextConfig} */
-module.exports = nextTranslate(
-  withBundleAnalyzer({
-    eslint: {
-      dirs: ['src'],
-    },
+module.exports = nextTranslate({
+  reactStrictMode: true,
 
-    reactStrictMode: true,
+  images: {
+    domains: [
+      'cdn.jsdelivr.net',
+      'avatars.githubusercontent.com',
+      'res.cloudinary.com',
+      'cdnjs.cloudflare.com',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
 
-    images: {
-      domains: [
-        'cdn.jsdelivr.net',
-        'avatars.githubusercontent.com',
-        'res.cloudinary.com',
-        'cdnjs.cloudflare.com',
-      ],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.googleusercontent.com',
-        },
-      ],
-    },
-
-    // SVGR
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              icon: true,
-            },
-          },
-        ],
-      })
-
-      return config
-    },
-
-    async redirects() {
-      return [
-        {
-          source: '/youtube',
-          destination:
-            'https://www.youtube.com/channel/UC2hMWOaOlk9vrkvFVaGmn0Q',
-          permanent: false,
-        },
-      ]
-    },
-  })
-)
+  async redirects() {
+    return [
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/channel/UC2hMWOaOlk9vrkvFVaGmn0Q',
+        permanent: false,
+      },
+    ]
+  },
+})
