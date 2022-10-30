98f1aca93ca577fdffef97f4990c9743fc93ac3e
tszhong0411 <lainelson411@gmail.com>
2022-10-30 19:11:37 +0800

    :recycle: Write the condition in Umami component

--- src/components/Umami/Umami.tsx
+++ src/components/Umami/Umami.tsx
@@ -1,16 +1,22 @@
 import Script from 'next/script'
 
-const UmamiScript = () => {
-  return (
-    <>
-      <Script
-        async
-        defer
-        data-website-id='f9234f5f-8348-47d2-8808-9dce193bb82c'
-        src='https://umami.honghong.me/umami.js' // Replace with your umami instance
-      />
-    </>
-  )
+import { isProd } from '@/lib/isProduction'
+
+const Umami = () => {
+  if (isProd) {
+    return (
+      <>
+        <Script
+          async
+          defer
+          data-website-id='f9234f5f-8348-47d2-8808-9dce193bb82c'
+          src='https://umami.honghong.me/umami.js' // Replace with your umami instance
+        />
+      </>
+    )
+  }
+
+  return null
 }
 
-export default UmamiScript
+export default Umami
