594f014487a8ff4a4d548fedeb07f4138c1a966b
tszhong0411 <lainelson411@gmail.com>
2022-11-01 22:29:43 +0800

    :recycle: Use if else instead of switch

--- src/pages/api/likes/[slug].ts
+++ src/pages/api/likes/[slug].ts
@@ -29,77 +29,69 @@ export default async function handler(
     // Identify a specific users interactions with a specific post
     const sessionId = slug + '___' + currentUserId
 
-    switch (req.method) {
-      case 'GET': {
-        const [post, user] = await Promise.all([
-          // get the number of likes this post has
-          prisma.post.findUnique({
-            where: { slug },
-          }),
+    if (req.method === 'GET') {
+      const [post, user] = await Promise.all([
+        // get the number of likes this post has
+        prisma.post.findUnique({
+          where: { slug },
+        }),
 
-          // get the number of times the current user has liked this post
-          prisma.session.findUnique({
-            where: { id: sessionId },
-          }),
-        ])
+        // get the number of times the current user has liked this post
+        prisma.session.findUnique({
+          where: { id: sessionId },
+        }),
+      ])
 
-        res.json({
-          likes: post?.likes || 0,
-          currentUserLikes: user?.likes || 0,
-        })
-
-        break
-      }
-
-      case 'POST': {
-        const count = z.number().min(1).max(3).parse(req.body.count)
-
-        // Upsert: if a row exists, update it by incrementing likes. If it
-        // doesn't exist, create a new row with the number of likes this api
-        // route receives
-        const [post, user] = await Promise.all([
-          // increment the number of times everyone has liked this post
-          prisma.post.upsert({
-            where: { slug },
-            create: {
-              slug,
-              likes: count,
-            },
-            update: {
-              likes: {
-                increment: count,
-              },
-            },
-          }),
-
-          // increment the number of times this user has liked this post
-          prisma.session.upsert({
-            where: { id: sessionId },
-            create: {
-              id: sessionId,
-              likes: count,
-            },
-            update: {
-              likes: {
-                increment: count,
-              },
-            },
-          }),
-        ])
-
-        res.json({
-          likes: post?.likes || 0,
-          currentUserLikes: user?.likes || 0,
-        })
-
-        break
-      }
-
-      default: {
-        res.setHeader('Allow', ['GET', 'POST'])
-        res.status(405).send('Method Not Allowed')
-      }
+      return res.json({
+        likes: post?.likes || 0,
+        currentUserLikes: user?.likes || 0,
+      })
     }
+
+    if (req.method === 'POST') {
+      const count = z.number().min(1).max(3).parse(req.body.count)
+
+      // Upsert: if a row exists, update it by incrementing likes. If it
+      // doesn't exist, create a new row with the number of likes this api
+      // route receives
+      const [post, user] = await Promise.all([
+        // increment the number of times everyone has liked this post
+        prisma.post.upsert({
+          where: { slug },
+          create: {
+            slug,
+            likes: count,
+          },
+          update: {
+            likes: {
+              increment: count,
+            },
+          },
+        }),
+
+        // increment the number of times this user has liked this post
+        prisma.session.upsert({
+          where: { id: sessionId },
+          create: {
+            id: sessionId,
+            likes: count,
+          },
+          update: {
+            likes: {
+              increment: count,
+            },
+          },
+        }),
+      ])
+
+      return res.json({
+        likes: post?.likes || 0,
+        currentUserLikes: user?.likes || 0,
+      })
+    }
+
+    res.setHeader('Allow', ['GET', 'POST'])
+    return res.status(405).send('Method Not Allowed')
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err.message)
