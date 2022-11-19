5c0e3192af7a96cb1702acb9a6cdf1f1b6e2e2f1
tszhong0411 <lainelson411@gmail.com>
2022-11-20 00:54:42 +0800

    :coffin: Remove useless code

--- src/components/Analytics/Analytics.tsx
+++ src/components/Analytics/Analytics.tsx
@@ -5,14 +5,12 @@ import { isProduction } from '@/lib/constants'
 const Analytics = () => {
   if (isProduction) {
     return (
-      <>
-        <Script
-          async
-          defer
-          data-website-id='f9234f5f-8348-47d2-8808-9dce193bb82c'
-          src='https://umami.honghong.me/umami.js' // Replace with your umami instance
-        />
-      </>
+      <Script
+        async
+        defer
+        data-website-id='f9234f5f-8348-47d2-8808-9dce193bb82c'
+        src='https://umami.honghong.me/umami.js' // Replace with your umami instance
+      />
     )
   }
 
