160d23c20eeb7044e4967dbc810c99692003edca
tszhong0411 <lainelson411@gmail.com>
2022-11-20 23:42:33 +0800

    :sparkles: Update projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,81 +1,127 @@
 import { Grid } from '@mantine/core'
-import { useRouter } from 'next/router'
+import { IconMarkdown } from '@tabler/icons'
 import useTranslation from 'next-translate/useTranslation'
 
+import {
+  IconNextJS,
+  IconPlanetScale,
+  IconPrisma,
+  IconTypescript,
+} from '@/components/Icon'
 import Layout from '@/components/Layout'
 import PageLayout from '@/components/Layout/PageLayout'
 import ProjectsCard from '@/components/ProjectCard'
-
-type ProjectData = {
-  [key: string]: {
-    title: string
-    description: string
-    href: string
-  }[]
-}
+import { ProjectCardProps } from '@/components/ProjectCard/ProjectCard'
 
 const Projects = () => {
-  const { locale } = useRouter()
   const { t } = useTranslation('common')
 
-  const projectsData: ProjectData = {
-    'zh-TW': [
-      {
-        title: 'Blog',
-        description: `分享我的知識與經驗`,
-        href: 'https://honghong.me',
-      },
-      {
-        title: '好友測驗作弊器',
-        description: '在好友測驗上取得滿分',
-        href: 'https://friendquiz.honghong.me',
-      },
-      {
-        title: '小康的社交媒體',
-        description: '展示更多小康的社交媒體',
-        href: 'https://link.honghong.me',
-      },
-      {
-        title: '小康 Tools',
-        description: 'Web 開發人員的一些工具',
-        href: 'https://tools.honghong.me',
-      },
-    ],
-    en: [
-      {
-        title: 'Blog',
-        description: `Share my knowledge and experience`,
-        href: 'https://honghong.me',
-      },
-      {
-        title: 'Friend quiz cheat tool',
-        description: 'Get full score in friend quiz',
-        href: 'https://friendquiz.honghong.me',
-      },
-      {
-        title: "小康's social media",
-        description: "Display more 小康's social media",
-        href: 'https://link.honghong.me',
-      },
-      {
-        title: '小康 Tools',
-        description: 'Some tools for web developer',
-        href: 'https://tools.honghong.me',
-      },
-    ],
-  }
+  const projectsData: ProjectCardProps[] = [
+    {
+      title: t('Projects.blog.title'),
+      description: t('Projects.blog.description'),
+      href: 'https://honghong.me',
+      image: '/static/images/projects/blog.png',
+      badges: [
+        {
+          icon: <IconTypescript strokeWidth={1.5} size={16} />,
+          label: 'Typescript',
+        },
+        {
+          icon: <IconPlanetScale strokeWidth={1.5} size={16} />,
+          label: 'PlanetScale',
+        },
+        {
+          icon: <IconNextJS strokeWidth={1.5} size={16} />,
+          label: 'Next.js',
+        },
+        {
+          icon: <IconPrisma strokeWidth={1.5} size={16} />,
+          label: 'Prisma',
+        },
+        {
+          icon: <IconMarkdown strokeWidth={1.5} size={16} />,
+          label: 'MDX',
+        },
+      ],
+    },
+    {
+      title: t('Projects.friendQuiz.title'),
+      description: t('Projects.friendQuiz.description'),
+      href: 'https://friendquiz.honghong.me',
+      image: '/static/images/projects/friend-quiz.png',
+      badges: [
+        {
+          icon: <IconTypescript fill='black' strokeWidth={1.5} size={16} />,
+          label: 'Typescript',
+        },
+        {
+          icon: <IconNextJS strokeWidth={1.5} size={16} />,
+          label: 'Next.js',
+        },
+      ],
+    },
+    {
+      title: t('Projects.link.title'),
+      description: t('Projects.link.description'),
+      href: 'https://link.honghong.me',
+      image: '/static/images/projects/link.png',
+      badges: [
+        {
+          icon: <IconTypescript fill='black' strokeWidth={1.5} size={16} />,
+          label: 'Typescript',
+        },
+        {
+          icon: <IconNextJS strokeWidth={1.5} size={16} />,
+          label: 'Next.js',
+        },
+      ],
+    },
+    {
+      title: t('Projects.tools.title'),
+      description: t('Projects.tools.description'),
+      href: 'https://tools.honghong.me',
+      image: '/static/images/projects/tools.png',
+      badges: [
+        {
+          icon: <IconTypescript fill='black' strokeWidth={1.5} size={16} />,
+          label: 'Typescript',
+        },
+        {
+          icon: <IconNextJS strokeWidth={1.5} size={16} />,
+          label: 'Next.js',
+        },
+      ],
+    },
+    {
+      title: t('Projects.oneBlog.title'),
+      description: t('Projects.oneBlog.description'),
+      href: 'https://one-blog.honghong.me',
+      image: '/static/images/projects/one-blog.png',
+      badges: [
+        {
+          icon: <IconTypescript fill='black' strokeWidth={1.5} size={16} />,
+          label: 'Typescript',
+        },
+        {
+          icon: <IconNextJS strokeWidth={1.5} size={16} />,
+          label: 'Next.js',
+        },
+        {
+          icon: <IconPrisma strokeWidth={1.5} size={16} />,
+          label: 'Prisma',
+        },
+      ],
+    },
+  ]
 
   return (
     <Layout title='Projects' description={t('Seo.projectsDesc')}>
       <PageLayout title='Projects' description={t('Seo.projectsDesc')}>
         <Grid>
-          {projectsData[locale]?.map(({ title, description, href }) => (
-            <Grid.Col key={title} span={12} md={6}>
-              <ProjectsCard
-                title={title}
-                description={description}
-                href={href}
-              />
+          {projectsData.map((project) => (
+            <Grid.Col key={project.title} span={12} md={6}>
+              <ProjectsCard {...project} />
             </Grid.Col>
           ))}
         </Grid>
