a7da017c8afe56a1eac9cbf2d854d8b47a08da3a
tszhong0411 <lainelson411@gmail.com>
2022-11-22 18:25:15 +0800

    :sparkles: Use experimental edge

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -1,18 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
 import { getNowPlaying } from '@/lib/spotify'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+const handler = async () => {
   const response = await getNowPlaying()
 
   if (response.status === 204 || response.status > 400) {
-    return res.status(200).json({ isPlaying: false })
+    return new Response(JSON.stringify({ isPlaying: false }), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
   }
 
   const song = await response.json()
 
   if (song.item === null) {
-    return res.status(200).json({ isPlaying: false })
+    return new Response(JSON.stringify({ isPlaying: false }), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
   }
 
   const isPlaying = song.is_playing
@@ -22,19 +34,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const albumImageUrl = song.item.album.images[0].url
   const songUrl = song.item.external_urls.spotify
 
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=60, stale-while-revalidate=30'
+  return new Response(
+    JSON.stringify({
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+        'cache-control': 'public, s-maxage=60, stale-while-revalidate=30',
+      },
+    }
   )
-
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  })
 }
 
 export default handler
