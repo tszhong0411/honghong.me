415f1a6deb8dd0a7a0b23dcd74d2b719a87f4bbc
tszhong0411 <lainelson411@gmail.com>
2022-11-23 00:00:30 +0800

    :white_check_mark: Fix typos

--- src/__tests__/I18n.test.ts
+++ src/__tests__/I18n.test.ts
@@ -3,7 +3,7 @@ import _ from 'lodash'
 import en from '../../locales/en/common.json'
 import zhTW from '../../locales/zh-TW/common.json'
 
-describe('@/components/Link', () => {
+describe('Internationalization', () => {
   it('Each language should have the same namespace', () => {
     expect(_.isEqual(Object.keys(zhTW), Object.keys(en))).toBe(true)
   })
