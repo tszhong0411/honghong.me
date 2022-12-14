ac0d3f8daa289bd732db56cec799ca3ce92445cb
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:40:37 +0800

    feat: add dashboard card

---
+++ src/components/Dashboard/Card/Card.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import CountUp from 'react-countup'
+
+import Link from '@/components/Link'
+import Skeleton from '@/components/Skeleton'
+
+type CardProps = {
+  icon: React.ReactNode
+  title: string
+  href: string
+  data: number
+}
+
+const Card = (props: CardProps) => {
+  const { icon, title, href, data } = props
+
+  return (
+    <Link
+      href={href}
+      icon={false}
+      animation={false}
+      className='flex flex-col gap-2 rounded-lg border border-accent-2 p-4 transition-colors duration-300 hover:bg-accent-1'
+    >
+      <div className='flex items-center gap-1'>
+        {icon}
+        <div className='text-sm font-bold'>{title}</div>
+      </div>
+      {data > 0 ? (
+        <div className='text-4xl font-black text-hong-fg'>
+          <CountUp start={0} end={data} separator=',' />
+        </div>
+      ) : (
+        <Skeleton className='h-10 w-1/3' />
+      )}
+    </Link>
+  )
+}
+
+export default Card
