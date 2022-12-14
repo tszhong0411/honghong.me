b18f68c9fce4e145b2675bc2751469091eb6c049
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:44:09 +0800

    feat: new style of like button

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -1,95 +1,105 @@
-import { ActionIcon, Center, Flex, Skeleton, Text } from '@mantine/core'
+'use client'
+
 import { motion } from 'framer-motion'
+import React from 'react'
+import Confetti from 'react-confetti'
+import { useWindowSize } from 'react-use'
 
-import { usePostLikes } from '@/hooks/usePostLikes'
+import { usePostLikes } from '@/hooks'
 
-import { useStyles } from './LikeButton.styles'
-
+import Skeleton from '../Skeleton'
 type LikeButtonProps = {
   slug: string
 }
 
-const emojis = ['❤️', '😘', '🥰']
-
 const LikeButton = (props: LikeButtonProps) => {
   const { slug } = props
+
   const { currentUserLikes, likes, isLoading, increment } = usePostLikes(slug)
-  const { classes } = useStyles()
+  const [position, setPosition] = React.useState<{ x: number; y: number }>()
+  const { width, height } = useWindowSize()
 
   return (
-    <Center mt={40}>
-      <Flex align='center' wrap='nowrap' gap={16}>
-        <ActionIcon
-          size='xl'
-          variant='transparent'
-          onClick={() => {
-            if (isLoading) return
-            increment()
-          }}
-          sx={{
-            position: 'relative',
-          }}
-        >
-          <div className={classes.emojiWrapper}>
-            {emojis.map((item, index) => {
-              return (
-                <motion.div
-                  key={index}
-                  className={classes.emoji}
-                  animate={currentUserLikes === index + 1 ? 'show' : 'hide'}
-                  variants={{
-                    hide: { translateY: -80, opacity: 0 },
-                    show: {
-                      translateY: [0, -40, -60],
-                      opacity: [0, 1, 0],
-                    },
-                  }}
-                  initial='hide'
-                >
-                  {item}
-                </motion.div>
-              )
-            })}
-          </div>
-          <svg viewBox='0 0 20 20' style={{ width: 96 }}>
-            <defs>
-              <linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='100%'>
-                <stop offset='20%' className={classes.firstStop}></stop>
-                <stop offset='80%' className={classes.secondStop}></stop>
-              </linearGradient>
-              <mask id='mask' mask-type='alpha' maskUnits='userSpaceOnUse'>
-                <path d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'></path>
-              </mask>
-            </defs>
-            <g mask='url(#mask)'>
-              <rect width='20' height='20' className={classes.heart}></rect>
-              <motion.rect
-                fill='url(#gradient)'
-                width='16'
-                height='16'
-                x='2'
-                y='2'
-                animate={String(currentUserLikes)}
-                variants={{
-                  '0': { translateY: 17 },
-                  '1': { translateY: 12 },
-                  '2': { translateY: 8 },
-                  '3': { translateY: 1 },
-                }}
-                initial='0'
-              ></motion.rect>
-            </g>
-          </svg>
-        </ActionIcon>
-        {!isLoading ? (
-          <Text size={22} weight={700}>
-            {likes}
-          </Text>
-        ) : (
-          <Skeleton width={50} height={25} />
+    <div className='mt-12 flex flex-nowrap items-center justify-center gap-4'>
+      <button
+        className='outline-none'
+        onClick={(e) => {
+          if (isLoading) return
+          if (currentUserLikes === 2) {
+            setPosition({ x: e.clientX, y: e.clientY })
+          }
+
+          increment()
+        }}
+      >
+        <svg viewBox='0 0 20 20' className='w-[42px]'>
+          <defs>
+            <linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='100%'>
+              <stop
+                offset='20%'
+                stopColor='rgb(250,51,81)'
+                stopOpacity={1}
+              ></stop>
+              <stop
+                offset='80%'
+                stopColor='rgb(255,121,44)'
+                stopOpacity={1}
+              ></stop>
+            </linearGradient>
+            <mask id='mask' mask-type='alpha' maskUnits='userSpaceOnUse'>
+              <path d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'></path>
+            </mask>
+          </defs>
+          <g mask='url(#mask)'>
+            <rect
+              width='20'
+              height='20'
+              className='fill-[#c4c1c1] dark:fill-[#4b5563]'
+            ></rect>
+            <motion.rect
+              fill='url(#gradient)'
+              width='16'
+              height='16'
+              x='2'
+              y='2'
+              animate={String(currentUserLikes)}
+              variants={{
+                '0': { translateY: 17 },
+                '1': { translateY: 12 },
+                '2': { translateY: 8 },
+                '3': { translateY: 1 },
+              }}
+              initial='0'
+            ></motion.rect>
+          </g>
+        </svg>
+        {!!position && (
+          <Confetti
+            style={{
+              position: 'fixed',
+              inset: 0,
+            }}
+            recycle={false}
+            width={width}
+            height={height}
+            confettiSource={{
+              x: position.x,
+              y: position.y,
+              h: 5,
+              w: 5,
+            }}
+            onConfettiComplete={() => {
+              setPosition(null)
+            }}
+          />
         )}
-      </Flex>
-    </Center>
+      </button>
+      {!isLoading ? (
+        <div className='text-[22px] font-bold'>{likes}</div>
+      ) : (
+        <Skeleton className='h-[25px] w-[50px]' />
+      )}
+    </div>
   )
 }
 
