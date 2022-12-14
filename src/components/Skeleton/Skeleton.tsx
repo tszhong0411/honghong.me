2486d2e7a20a79997aac9699f36a8672f1a69ca9
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:33:31 +0800

    feat: add skeleton

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,20 @@
+import clsxm from '@/lib/clsxm'
+
+import { WithClassName } from '@/types'
+
+type SkeletonProps = WithClassName
+
+const Skeleton = (props: SkeletonProps) => {
+  const { className } = props
+
+  return (
+    <div
+      className={clsxm(
+        'h-4 w-full animate-pulse rounded-md bg-zinc-200 dark:bg-zinc-800',
+        className
+      )}
+    />
+  )
+}
+
+export default Skeleton
