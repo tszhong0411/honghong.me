a760cbc32545d201e421da697c519627ed9e2368
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:09:43 +0800

    feat: add config files

---
+++ src/config/giscus.ts
@@ -0,0 +1,14 @@
+import { GiscusProps } from '@giscus/react'
+
+export const GISCUS_CONFIG: GiscusProps = {
+  repo: 'tszhong0411/honghong.me',
+  repoId: 'R_kgDOGxHFnA',
+  category: 'Blog Comments',
+  categoryId: 'DIC_kwDOGxHFnM4CBGIQ',
+  mapping: 'pathname',
+  reactionsEnabled: '1',
+  emitMetadata: '1',
+  inputPosition: 'bottom',
+  lang: 'en',
+  loading: 'eager',
+}
