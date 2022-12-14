2d7f4438e33e7626f1aee3a3999d4b9772ce4573
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:39:34 +0800

    feat: update mdx components

--- src/components/MDXComponents/MDXComponents.tsx
+++ src/components/MDXComponents/MDXComponents.tsx
@@ -1,77 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  Alert as MantineAlert,
-  AlertProps,
-  Blockquote,
-  Box,
-  Center,
-  Kbd,
-  Table,
-  TableProps,
-  Text,
-} from '@mantine/core'
-import Image, { ImageProps } from 'next/legacy/image'
-import React from 'react'
+import Link from '@/components/Link'
 
-import Files from '@/components/Files'
-import CustomLink from '@/components/Link'
-
-import ItemGrid from '../ItemGrid'
-
-const PostImage = ({ alt, src, ...rest }: ImageProps) => (
-  <Box
-    component='figure'
-    sx={{
-      '& img': {
-        borderRadius: 12,
-      },
-    }}
-  >
-    <Center>
-      <Image
-        alt={alt}
-        src={src}
-        blurDataURL={`/_next/image?url=${src}&w=16&q=1`}
-        placeholder='blur'
-        {...rest}
-      />
-    </Center>
-    <Text
-      component='figcaption'
-      size='sm'
-      align='center'
-      sx={(theme) => ({
-        color:
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[2]
-            : theme.colors.gray[7],
-        marginTop: theme.spacing.xs,
-      })}
-    >
-      {alt}
-    </Text>
-  </Box>
-)
-
-const PostTable = (props: TableProps) => (
-  <Table highlightOnHover>{props.children}</Table>
-)
-
-const Alert = ({ children, ...rest }: AlertProps) => (
-  <MantineAlert my={16} {...rest} variant='outline'>
-    {children}
-  </MantineAlert>
-)
+import Callout from './Callout'
+import Image from './Image'
+import ItemGrid from './ItemGrid'
+import Kbd from './Kbd'
+import Pre from './Pre'
+import Tree from './Tree'
+import Colors from '../Design/Colors'
 
 const MDXComponents = {
-  Kbd,
-  Blockquote,
-  Alert,
-  Files,
-  a: CustomLink,
-  table: PostTable,
-  Image: PostImage,
+  a: Link,
+  Image,
+  pre: Pre,
+
+  // Custom components
   ItemGrid,
+  Callout,
+  Kbd,
+  Tree,
+  Colors,
 }
 
 export default MDXComponents
