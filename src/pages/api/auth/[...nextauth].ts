1222bde2ed69d3afcf9ff251adbb4febe5f266b6
tszhong0411 <lainelson411@gmail.com>
2022-12-15 00:58:50 +0800

    refactor: move nextauth config to config folder

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,5 @@
-import type { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GithubProvider({
-      clientId: process.env.OAUTH_CLIENT_KEY,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-}
+import { authOptions } from '@/lib/auth'
 
 export default NextAuth(authOptions)
