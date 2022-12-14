4512ed8b31f42c1df1cfd059ef2b614eec6286c2
tszhong0411 <lainelson411@gmail.com>
2022-12-15 01:39:20 +0800

    feat: add toaster

---
+++ src/components/CustomToaster/CustomToaster.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Toaster } from 'react-hot-toast'
+
+const CustomToaster = () => {
+  return (
+    <Toaster
+      position='bottom-right'
+      toastOptions={{
+        className: 'bg-accent-1 text-hong-fg border border-accent-2',
+      }}
+    />
+  )
+}
+
+export default CustomToaster
