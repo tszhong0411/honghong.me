4c1d88cee02a4be72b95786295a553092e525d2d
tszhong0411 <lainelson411@gmail.com>
2022-12-15 22:51:06 +0800

    feat: update logo

--- src/components/Layout/Header/HeaderLogo/HeaderLogo.tsx
+++ src/components/Layout/Header/HeaderLogo/HeaderLogo.tsx
@@ -5,12 +5,7 @@ import Logo from '@/components/Logo'
 const HeaderLogo = () => {
   return (
     <Link href='/' className='flex items-center justify-center gap-1'>
-      <Logo
-        width={28}
-        height={28}
-        className='fill-[#8b1d1d] dark:fill-[#c92a2a]'
-      />
-      <span className='leading-7'>小康</span>
+      <Logo width={28} height={28} />
     </Link>
   )
 }
--- src/lib/favicons.ts
+++ src/lib/favicons.ts
@@ -8,59 +8,12 @@ type Favicon = {
 export const favicons: Favicon[] = [
   {
     rel: 'icon',
-    type: 'image/x-icon',
-    href: '/static/favicon/favicon.ico',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/static/favicon/apple-icon-57x57.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/static/favicon/apple-icon-60x60.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/static/favicon/apple-icon-72x72.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/static/favicon/apple-icon-76x76.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/static/favicon/apple-icon-114x114.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/static/favicon/apple-icon-120x120.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/static/favicon/apple-icon-144x144.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/static/favicon/apple-icon-152x152.png',
+    href: '/static/favicon/favicon.svg',
   },
   {
     rel: 'apple-touch-icon',
     sizes: '180x180',
-    href: '/static/favicon/apple-icon-180x180.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/static/favicon/android-icon-192x192.png',
+    href: '/static/favicon/apple-touch-icon.png',
   },
   {
     rel: 'icon',
@@ -75,13 +28,7 @@ export const favicons: Favicon[] = [
     href: '/static/favicon/favicon-32x32.png',
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '96x96',
-    href: '/static/favicon/favicon-96x96.png',
-  },
-  {
     rel: 'manifest',
-    href: '/static/favicon/manifest.json',
+    href: '/static/favicon/site.webmanifest',
   },
 ]
