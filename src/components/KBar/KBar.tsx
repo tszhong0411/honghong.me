420972aed11c1b0af62f81b6df7a20fd1ac361e2
tszhong0411 <lainelson411@gmail.com>
2023-02-18 23:39:10 +0800

    refactor: 使用中文作為網站語言

--- src/components/KBar/KBar.tsx
+++ src/components/KBar/KBar.tsx
@@ -36,26 +36,26 @@ const KBar = (props: KBarProps) => {
   const actions: Action[] = [
     {
       id: 'copy-link',
-      name: 'Copy Link',
-      keywords: 'copy link',
-      section: 'General',
+      name: '複製連結',
+      keywords: '連結',
+      section: '主要',
       perform: async () => {
         if (!navigator?.clipboard) {
-          toast.error('Access to clipboard rejected!')
+          toast.error('存取剪貼板被拒絕')
         }
 
         try {
           await navigator.clipboard.writeText(window.location.href)
           toast.success(
             <div className='flex flex-col'>
-              <div>Copied</div>
+              <div>已複製</div>
               <div className='text-sm text-accent-5'>
-                You can now share it with anyone.
+                你現在可以分享給任何人
               </div>
             </div>
           )
         } catch {
-          toast.error('Failed to copy!')
+          toast.error('複製失敗！')
         }
       },
       icon: <IconLink />,
@@ -63,26 +63,26 @@ const KBar = (props: KBarProps) => {
 
     {
       id: 'source-code',
-      name: 'Source code',
-      keywords: 'source code github',
-      section: 'General',
+      name: '原始碼',
+      keywords: '原始碼 程式碼 github',
+      section: '主要',
       perform: () =>
         window.open('https://github.com/tszhong0411/honghong.me', '_blank'),
       icon: <IconCode />,
     },
     {
       id: 'light-mode',
-      name: 'Light Mode',
-      keywords: 'light mode',
-      section: 'Settings',
+      name: '明亮模式',
+      keywords: '明亮 模式',
+      section: '設置',
       perform: () => setTheme('light'),
       icon: <IconSun />,
     },
     {
       id: 'dark-mode',
-      name: 'Dark Mode',
-      keywords: 'dark mode',
-      section: 'Settings',
+      name: '黑暗模式',
+      keywords: '黑暗 模式',
+      section: '設置',
       perform: () => setTheme('dark'),
       icon: <IconMoon />,
     },
@@ -90,7 +90,7 @@ const KBar = (props: KBarProps) => {
       id: 'github',
       name: 'GitHub',
       keywords: 'github',
-      section: 'Social',
+      section: '社交',
       perform: () => window.open('https://github.com/tszhong0411', '_blank'),
       icon: <IconBrandGithub />,
     },
@@ -98,7 +98,7 @@ const KBar = (props: KBarProps) => {
       id: 'instagram',
       name: 'Instagram',
       keywords: 'ig instagram',
-      section: 'Social',
+      section: '社交',
       perform: () =>
         window.open('https://instagram.com/tszhong0411/', '_blank'),
       icon: <IconBrandInstagram />,
@@ -107,7 +107,7 @@ const KBar = (props: KBarProps) => {
       id: 'youtube',
       name: 'YouTube',
       keywords: 'yt youtube',
-      section: 'Social',
+      section: '社交',
       perform: () => window.open('https://youtube.com/@tszhong0411', '_blank'),
       icon: <IconBrandYoutube />,
     },
@@ -115,7 +115,7 @@ const KBar = (props: KBarProps) => {
       id: 'facebook',
       name: 'Facebook',
       keywords: 'fb facebook',
-      section: 'Social',
+      section: '社交',
       perform: () =>
         window.open('https://www.facebook.com/tszhonglai.0411/', '_blank'),
       icon: <IconBrandFacebook />,
