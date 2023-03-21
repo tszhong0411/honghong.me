333da7b0db6234c74dcf47f650ac7e151d28f5d6
tszhong0411 <lainelson411@gmail.com>
2023-03-21 19:59:58 +0800

    feat: 更新用詞

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -22,7 +22,7 @@ const BlogPage = () => {
       <div className='mb-8 space-y-8'>
         <h2 className='my-4 text-4xl font-bold'>Blog</h2>
         <p className='text-accent-5'>
-          我在 2020 年 12 月開始寫文章，主要是軟件和分享知識。我總共寫了{' '}
+          我在 2020 年 12 月開始寫文章，主要是軟體和分享知識。我總共寫了{' '}
           {posts.length} 篇文章在我的 blog。你可以用標題搜尋文章在下面的搜尋框。
         </p>
       </div>
