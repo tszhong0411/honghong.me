d13dc6b33d7caaba1ba59b2f9f19d791aae0ee15
tszhong0411 <lainelson411@gmail.com>
2023-03-22 00:56:06 +0800

    feat: 更新 404 頁面

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 const NotFound = () => {
   return (
     <div className='mt-52 mb-40 flex flex-col items-center justify-center gap-12'>
-      <h1 className='text-center text-6xl font-bold'>404 Page Not Found</h1>
+      <h1 className='text-center text-6xl font-bold'>404 頁面不存在</h1>
       <Link
         href='/'
         className='animated rounded-lg border border-accent-2 px-3 py-2 transition-colors duration-300'
