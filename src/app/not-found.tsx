2f850fd60ca4a057670638d2239fd827f35cc86d
tszhong0411 <lainelson411@gmail.com>
2023-04-08 14:14:32 +0800

    fix: 404 page button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ const NotFound = () => {
       <h1 className='text-center text-6xl font-bold'>404 頁面不存在</h1>
       <Link
         href='/'
-        className='animated rounded-lg border border-accent-2 px-3 py-2 transition-colors duration-300'
+        className='rounded-lg border border-accent-2 px-3 py-2 transition-colors duration-300 hover:bg-accent-2'
       >
         回到首頁
       </Link>
