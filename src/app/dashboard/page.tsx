c54380817a745d0aa71198e6e680f0477880fdb4
tszhong0411 <lainelson411@gmail.com>
2023-05-14 18:40:30 +0800

    feat: add revalidation to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,8 @@ import { site } from '@/config/site'
 
 import Items from './items'
 
+export const revalidate = 60
+
 const title = 'Dashboard'
 const description =
   'This is my personal dashboard, built with Next.js API routes deployed as serverless functions. I use this dashboard to track various metrics across platforms like YouTube, GitHub, and more.'
