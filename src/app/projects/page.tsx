cd5aaa19a455a09c64677ddaef5bf7e079969013
tszhong0411 <lainelson411@gmail.com>
2023-08-19 17:43:41 +0800

    feat: add special effect to project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,10 @@
 import { allProjects } from 'contentlayer/generated'
 import type { Metadata, ResolvingMetadata } from 'next'
-import Link from 'next/link'
 
-import Image from '@/components/mdx/image'
 import PageTitle from '@/components/page-title'
+import ProjectCard from '@/components/project-card'
 
 import { site } from '@/config/site'
-import getIconByName from '@/utils/get-icon-by-name'
 
 const title = 'Projects'
 const description = 'The list of my projects.'
@@ -51,48 +49,9 @@ const ProjectsPage = () => {
         description='The list of my projects. Everything was made with ❤️.'
       />
       <div className='flex flex-col gap-4'>
-        {allProjects.map((project) => {
-          const { _id, name, image, description, techstack, slug } = project
-
-          return (
-            <Link
-              key={_id}
-              href={`/projects/${slug}`}
-              className='flex flex-col rounded-lg border border-accent-2 p-4 transition-all duration-150 hover:scale-105 hover:bg-accent-1 md:flex-row'
-            >
-              <Image
-                src={image}
-                width={1200}
-                height={630}
-                alt={name}
-                className='rounded-lg md:w-72'
-              />
-              <div className='flex-1 px-2 py-4 md:px-4 md:py-2'>
-                <div>
-                  <h2 className='text-2xl font-bold text-accent-fg'>{name}</h2>
-                  <div className='text-accent-5'>{description}</div>
-                </div>
-                <div className='mt-[5px] flex flex-wrap gap-[7px]'>
-                  {techstack.map((techstack) => {
-                    const { label } = techstack
-
-                    const Icon = getIconByName(label)
-
-                    return (
-                      <div
-                        key={label}
-                        className='flex items-center justify-center gap-1 rounded-full border border-accent-2 px-3 py-2'
-                      >
-                        <Icon strokeWidth={1.5} size={16} />
-                        <div className='text-xs leading-4'>{label}</div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </Link>
-          )
-        })}
+        {allProjects.map((project) => (
+          <ProjectCard key={project._id} {...project} />
+        ))}
       </div>
     </>
   )
---
+++ src/components/project-card.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { Project } from 'contentlayer/generated'
+import { useMotionTemplate, useMotionValue } from 'framer-motion'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
+import getIconByName from '@/utils/get-icon-by-name'
+
+import Image from './mdx/image'
+
+type ProjectCardProps = Project
+
+const ProjectCard = (props: ProjectCardProps) => {
+  const { _id, name, image, description, techstack, slug } = props
+
+  const mouseX = useMotionValue(0)
+  const mouseY = useMotionValue(0)
+
+  return (
+    <Link
+      key={_id}
+      href={`/projects/${slug}`}
+      className='group relative flex flex-col rounded-lg border border-accent-2 p-4 transition-all duration-150 hover:scale-105 md:flex-row'
+      onMouseMove={(e) => {
+        const { left, top } = e.currentTarget.getBoundingClientRect()
+
+        mouseX.set(e.clientX - left)
+        mouseY.set(e.clientY - top)
+      }}
+    >
+      <motion.div
+        className='pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 [--spotlight:rgba(0,0,0,0.05)] group-hover:opacity-100 dark:[--spotlight:rgba(255,255,255,0.15)]'
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              var(--spotlight),
+              transparent 80%
+            )
+          `,
+        }}
+      />
+      <Image
+        src={image}
+        width={1200}
+        height={630}
+        alt={name}
+        className='rounded-lg md:w-72'
+      />
+      <div className='flex-1 px-2 py-4 md:px-4 md:py-2'>
+        <div>
+          <h2 className='text-2xl font-bold text-accent-fg'>{name}</h2>
+          <div className='text-accent-5'>{description}</div>
+        </div>
+        <div className='mt-[5px] flex flex-wrap gap-[7px]'>
+          {techstack.map((techstack) => {
+            const { label } = techstack
+
+            const Icon = getIconByName(label)
+
+            return (
+              <div
+                key={label}
+                className='flex items-center justify-center gap-1 rounded-full border border-accent-2 px-3 py-2'
+              >
+                <Icon strokeWidth={1.5} size={16} />
+                <div className='text-xs leading-4'>{label}</div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default ProjectCard
