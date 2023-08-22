22c17745589b0e95b612d3fc3e49b85fe6401404
tszhong0411 <lainelson411@gmail.com>
2023-08-22 18:49:46 +0800

    feat: update table

--- src/components/mdx/mdx.tsx
+++ src/components/mdx/mdx.tsx
@@ -1,6 +1,13 @@
 'use client'
 
-import { Alert, AlertDescription, AlertTitle, Kbd } from '@tszhong0411/ui'
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+  Kbd,
+  ScrollArea,
+  Scrollbar,
+} from '@tszhong0411/ui'
 import { MDXComponents } from 'mdx/types'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
@@ -34,6 +41,41 @@ const components: MDXComponents = {
     )
   },
   pre: Pre,
+  table: (props: React.ComponentPropsWithoutRef<'table'>) => {
+    const { children, ...rest } = props
+
+    return (
+      <div className='not-prose overflow-hidden rounded-lg border border-accent-2'>
+        <ScrollArea>
+          <table
+            className='w-full border-collapse border-spacing-0 text-left [&>tbody>tr:not(:last-child)>td]:border-b [&>tbody>tr:not(:last-child)>td]:border-accent-2'
+            {...rest}
+          >
+            {children}
+          </table>
+          <Scrollbar orientation='horizontal' />
+        </ScrollArea>
+      </div>
+    )
+  },
+  th: (props: React.ComponentPropsWithoutRef<'th'>) => {
+    const { children, ...rest } = props
+
+    return (
+      <th className='border-b border-accent-2 bg-zinc-900 p-3' {...rest}>
+        {children}
+      </th>
+    )
+  },
+  td: (props: React.ComponentPropsWithoutRef<'td'>) => {
+    const { children, ...rest } = props
+
+    return (
+      <td className='bg-zinc-950 p-3' {...rest}>
+        {children}
+      </td>
+    )
+  },
 
   // Custom components
   Alert: (props: React.ComponentPropsWithoutRef<typeof Alert>) => (
