08957da078b5c9b9beca4e87ff2d5cceb2c3a11e
tszhong0411 <lainelson411@gmail.com>
2023-08-28 02:05:54 +0800

    fix: non-clickable margin

--- src/components/image-zoom.tsx
+++ src/components/image-zoom.tsx
@@ -4,16 +4,17 @@ import 'react-medium-image-zoom/dist/styles.css'
 
 type ImageZoomProps = {
   children: React.ReactNode
-}
+} & React.ComponentPropsWithoutRef<typeof Zoom>
 
 const ImageZoom = (props: ImageZoomProps) => {
-  const { children } = props
+  const { children, ...rest } = props
 
   return (
     <Zoom
       overlayBgColorStart='rgba(0, 0, 0, 0)'
       overlayBgColorEnd='rgba(0, 0, 0, 0.8)'
       zoomMargin={40}
+      {...rest}
     >
       {children}
     </Zoom>
