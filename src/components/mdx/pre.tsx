0f48bad23c9ff3a08cbedc41f7e141ad12b328d2
tszhong0411 <lainelson411@gmail.com>
2023-09-11 01:20:03 +0800

    refactor: copy-button

--- src/components/mdx/pre.tsx
+++ src/components/mdx/pre.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { IconCheck, IconCopy } from '@tabler/icons-react'
 import React from 'react'
-import { toast } from 'react-hot-toast'
+
+import CopyButton from '../copy-button'
 
 type PreProps = React.ComponentPropsWithoutRef<'pre'>
 
@@ -10,50 +10,21 @@ const Pre = (props: PreProps) => {
   const { children, ...rest } = props
 
   const textInput = React.useRef<HTMLPreElement>(null)
-  const [isCopied, setCopied] = React.useState(false)
 
-  const onCopy = async () => {
-    setCopied(true)
-
-    if (!navigator?.clipboard) {
-      toast.error('Access to clipboard rejected!')
-    }
-
-    try {
-      if (textInput.current?.textContent) {
-        await navigator.clipboard.writeText(textInput.current.textContent)
-        toast.success('Copied')
-      }
-    } catch {
-      toast.error('Failed to copy!')
-    }
-  }
+  const [text, setText] = React.useState<string>('')
 
   React.useEffect(() => {
-    if (!isCopied) return
-
-    const timerId = setTimeout(() => {
-      setCopied(false)
-    }, 2000)
-
-    return () => {
-      clearTimeout(timerId)
+    if (textInput.current) {
+      setText(textInput.current.textContent || '')
     }
-  }, [isCopied])
+  }, [])
 
   return (
     <>
       <pre ref={textInput} {...rest}>
         {children}
       </pre>
-      <button
-        className='absolute right-2 top-2 flex h-8 w-8 items-center justify-center rounded-md border bg-background opacity-0 transition [div:hover>&]:opacity-100'
-        onClick={onCopy}
-        type='button'
-        aria-label='Copy code to clipboard'
-      >
-        {isCopied ? <IconCheck size={16} /> : <IconCopy size={16} />}
-      </button>
+      <CopyButton text={text} />
     </>
   )
 }
