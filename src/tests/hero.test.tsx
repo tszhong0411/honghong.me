5264dd4d185063729fa090854795bcbeb142b6bd
tszhong0411 <lainelson411@gmail.com>
2023-09-13 16:44:02 +0800

    test: update test, use jsdom, add test: ui, coverage

--- src/tests/hero.test.tsx
+++ src/tests/hero.test.tsx
@@ -1,15 +1,26 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, test } from 'vitest'
 
 import Hero from '@/components/hero'
 import { HERO_LINKS } from '@/config/links'
 
-describe('hero', () => {
-  test('social links', () => {
+describe('<Hero />', () => {
+  it('should have five social links', () => {
     render(<Hero />)
 
     for (const link of HERO_LINKS) {
-      expect(screen.getByLabelText(link.label)).toBeDefined()
+      expect(screen.getByLabelText(link.label)).toBeInTheDocument()
     }
+  })
+
+  it('should have a hero image', () => {
+    render(<Hero />)
+
+    expect(screen.getByAltText('Hong')).toBeInTheDocument()
+  })
+
+  it('should have a hero title', () => {
+    render(<Hero />)
+
+    expect(screen.getByText('Hong')).toBeInTheDocument()
   })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'happy-dom'
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts'
   },
   resolve: {
     alias: {
