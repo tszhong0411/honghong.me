0067a5cf42d538ed8c0447a4c0b55f6d74b790dc
tszhong0411 <lainelson411@gmail.com>
2023-09-15 13:57:19 +0800

    fix: lint error

--- src/tests/components/ui/command.test.tsx
+++ src/tests/components/ui/command.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 
 import {
   Command,
