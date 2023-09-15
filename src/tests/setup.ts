9ec5851d8b37170d0bddf5fed85892fe6387c313
tszhong0411 <lainelson411@gmail.com>
2023-09-15 14:15:03 +0800

    test: fix test

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,7 +2,20 @@ import '@testing-library/jest-dom'
 
 import { server } from './mocks/server'
 
-beforeAll(() => server.listen())
+beforeAll(() => {
+  vi.stubEnv('NEXT_PUBLIC_GISCUS_REPO', 'test')
+  vi.stubEnv('NEXT_PUBLIC_GISCUS_REPOSITORY_ID', 'test')
+  vi.stubEnv('NEXT_PUBLIC_GISCUS_CATEGORY', 'test')
+  vi.stubEnv('NEXT_PUBLIC_GISCUS_CATEGORY_ID', 'test')
+  vi.stubEnv('NEXT_PUBLIC_UMAMI_URL', 'http://test')
+  vi.stubEnv(
+    'NEXT_PUBLIC_UMAMI_WEBSITE_ID',
+    '00000000-0000-0000-0000-000000000000'
+  )
+  vi.stubEnv('NEXT_PUBLIC_UMAMI_WEBSITE_SHARE_URL', 'http://test')
+
+  server.listen()
+})
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
