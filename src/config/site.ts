148f03556dbd333de448e6d7d206eeb66d67bdd8
tszhong0411 <lainelson411@gmail.com>
2023-09-16 17:12:39 +0800

    feat: update site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -14,7 +14,7 @@ type Site = {
 const site: Site = {
   url:
     process.env.NODE_ENV === 'production'
-      ? 'https://honghong.me'
+      ? process.env.NEXT_PUBLIC_VERCEL_URL || 'https://honghong.me'
       : 'http://localhost:3000',
   title: 'Hong',
   name: 'Hong',
