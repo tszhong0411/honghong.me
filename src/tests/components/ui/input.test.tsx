a6c13b1be45c6bf5cbf4b148ec6b901e8b55081e
tszhong0411 <me@honghong.me>
2023-09-17 22:07:57 +0800

    test: fix typo

--- src/tests/components/ui/input.test.tsx
+++ src/tests/components/ui/input.test.tsx
@@ -29,7 +29,7 @@ describe('<Input />', () => {
     expect(input).not.toHaveFocus()
   })
 
-  it('shoud be focused when the label is clicked', async () => {
+  it('should be focused when the label is clicked', async () => {
     render(
       <>
         <Label htmlFor='test'>Test</Label>
--- src/tests/components/ui/textarea.test.tsx
+++ src/tests/components/ui/textarea.test.tsx
@@ -29,7 +29,7 @@ describe('<Textarea />', () => {
     expect(textarea).not.toHaveFocus()
   })
 
-  it('shoud be focused when the label is clicked', async () => {
+  it('should be focused when the label is clicked', async () => {
     render(
       <>
         <Label htmlFor='test'>Test</Label>
