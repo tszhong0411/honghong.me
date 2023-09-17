77ba1bcfa3dc7abb2b62896453efedbd61bc6f32
tszhong0411 <me@honghong.me>
2023-09-17 22:07:25 +0800

    refactor: add jsdoc

--- src/hooks/use-scrollspy.ts
+++ src/hooks/use-scrollspy.ts
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/**
+ * A hook that tracks which element is currently in view based on scroll position.
+ * @function useScrollspy
+ * @param ids An array of element IDs to track.
+ * @param options Options for the IntersectionObserver.
+ * @returns The ID of the currently active element.
+ */
 const useScrollspy = (ids: string[], options: IntersectionObserverInit) => {
   const [activeId, setActiveId] = React.useState<string>()
   const observer = React.useRef<IntersectionObserver>()
