eac2837ece2165c41fc2565d64dc16bf1372cc27
tszhong0411 <me@honghong.me>
2023-09-19 16:34:33 +0800

    refactor: update CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
 import { NextResponse } from 'next/server'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const middleware = () => {
   const ContentSecurityPolicy = `
+    base-uri 'self';
+    form-action 'self';
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.honghong.me vercel.live data: blob:;
-    style-src 'self' 'unsafe-inline';
-    img-src * blob: data:;
-    media-src *;
-    connect-src *;
+    script-src 'self'${
+      isProd ? '' : " 'unsafe-eval' 'unsafe-inline'"
+    } giscus.app *.honghong.me vercel.live;
+    style-src 'self'${isProd ? '' : " 'unsafe-inline'"};
+    connect-src 'self';
+    img-src 'self' blob: data:;
     font-src 'self';
-    frame-src giscus.app vercel.live;
+    media-src *;
+    frame-src *;
   `
 
   const response = NextResponse.next()
@@ -27,9 +33,10 @@ const middleware = () => {
     'Strict-Transport-Security',
     'max-age=31536000; includeSubDomains; preload'
   )
-  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN')
   response.headers.set('X-Content-Type-Options', 'nosniff')
   response.headers.set('X-DNS-Prefetch-Control', 'on')
+  response.headers.set('X-XSS-Protection', '1; mode=block')
 
   return response
 }
