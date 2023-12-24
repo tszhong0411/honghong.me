4ebb7f1a77c310ec49626c2239c118fb836385e2
tszhong0411 <me@honghong.me>
2023-12-24 21:50:22 +0800

    fix: current visitors

--- src/components/current-visitors.tsx
+++ src/components/current-visitors.tsx
@@ -26,7 +26,7 @@ const CurrentVisitors = () => {
             <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
             <span className='relative inline-flex h-3 w-3 rounded-full bg-green-500' />
           </span>
-          {data.visitors} current visitors
+          {data.visitors} current visitor{data.visitors > 1 && 's'}
         </>
       ) : (
         <Skeleton
