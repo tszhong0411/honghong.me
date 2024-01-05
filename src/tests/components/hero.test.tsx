cc94cedcf35bd5350747d4337a809d08f5967899
tszhong0411 <me@honghong.me>
2024-01-06 01:59:26 +0800

    test: update

--- src/tests/components/hero.test.tsx
+++ src/tests/components/hero.test.tsx
@@ -1,26 +1,11 @@
 import { render, screen } from '@testing-library/react'
 
-import Hero from '@/components/hero'
-import { HERO_LINKS } from '@/config/links'
+import Hero from '@/components/home/hero'
 
 describe('<Hero />', () => {
-  it('should have five social links', () => {
-    render(<Hero />)
-
-    for (const link of HERO_LINKS) {
-      expect(screen.getByLabelText(link.label)).toBeInTheDocument()
-    }
-  })
-
   it('should have a hero image', () => {
     render(<Hero />)
 
     expect(screen.getByAltText('Hong')).toBeInTheDocument()
-  })
-
-  it('should have a hero title', () => {
-    render(<Hero />)
-
-    expect(screen.getByText('Hong')).toBeInTheDocument()
   })
 })
