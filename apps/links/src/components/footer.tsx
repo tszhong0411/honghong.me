b3c62aa180f4942dd7ec56e3e89f9e83b104c12d
tszhong0411 <me@honghong.me>
2024-02-08 19:24:24 +0800

    chore: remove links from the repo

--- apps/links/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='mb-8 mt-16 flex justify-center'>
-      &copy; {new Date().getFullYear()} Hong
-    </footer>
-  )
-}
-
-export default Footer
