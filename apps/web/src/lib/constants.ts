bdb0b557b96d0a32ef0817aef6b0729ca9be548e
tszhong0411 <me@honghong.me>
2024-02-13 23:04:31 +0800

    fix(web): wrong web app url

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,8 +1,7 @@
-const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : ''
-
-export const WEBAPP_URL = VERCEL_URL || 'http://localhost:3000'
+export const WEBAPP_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://honghong.me'
+    : 'http://localhost:3000'
 
 export const GITHUB_USERNAME = 'tszhong0411'
 
