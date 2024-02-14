7eb4cc8dc6f15c00523c3f6688eae61aad273e31
tszhong0411 <me@honghong.me>
2024-02-14 19:11:17 +0800

    feat(web): adjust the opacity of the pinned message background

--- apps/web/src/app/guestbook/pinned.tsx
+++ apps/web/src/app/guestbook/pinned.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 const Pinned = () => {
   return (
     <div className='relative mb-12 overflow-hidden rounded-lg px-4 py-6'>
-      <div className='absolute inset-0 -z-10 rounded-lg bg-[linear-gradient(119deg,rgb(75,87,196)0%,rgb(31,133,206)30%,rgb(125,20,166)42%,rgb(134,15,60)63%,rgb(18,8,45)73%)] opacity-80' />
+      <div className='absolute inset-0 -z-10 rounded-lg bg-[linear-gradient(119deg,rgb(75,87,196)0%,rgb(31,133,206)30%,rgb(125,20,166)42%,rgb(134,15,60)63%,rgb(18,8,45)73%)] opacity-60' />
       <div className='mb-3 flex items-center gap-2'>
         <IconPinnedFilled className='rotate-45' />
         Pinned
