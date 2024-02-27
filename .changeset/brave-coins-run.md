0a6fafb51f39452ac4825df4192e3b3fd2650764
tszhong0411 <me@honghong.me>
2024-02-27 21:17:23 +0800

    feat(ui): update deps and export button props type

---
+++ .changeset/brave-coins-run.md
@@ -0,0 +1,5 @@
+---
+"@tszhong0411/ui": patch
+---
+
+update deps and export button props type
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -7,8 +7,8 @@ import * as React from 'react'
 
 export const buttonVariants = cva(
   [
-    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors',
-    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+    'ring-offset-background inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors',
+    'focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
     'disabled:pointer-events-none disabled:opacity-50'
   ],
   {
@@ -18,7 +18,7 @@ export const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
@@ -38,7 +38,7 @@ export const buttonVariants = cva(
   }
 )
 
-type ButtonProps = {
+export type ButtonProps = {
   asChild?: boolean
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>
