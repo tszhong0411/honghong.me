dc7ad298cc2c3768828b73e5adb82d3f61f05a0d
tszhong0411 <me@honghong.me>
2024-02-29 19:40:15 +0800

    feat(ui): add collapsible and files components

---
+++ packages/ui/src/collapsible.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
+
+export const Collapsible = CollapsiblePrimitive.Root
+
+export const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+export const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
---
+++ packages/ui/src/files.tsx
@@ -0,0 +1,74 @@
+/**
+ * Adapted from: https://github.com/fuma-nama/fumadocs/blob/cff6273b7d66377877d3d9d5ef6b6fd4ac20bac3/packages/ui/src/components/files.tsx
+ */
+'use client'
+
+import { cn } from '@tszhong0411/utils'
+import { cva } from 'class-variance-authority'
+import { FileIcon, FolderIcon, FolderOpenIcon } from 'lucide-react'
+import * as React from 'react'
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from './collapsible'
+
+type FilesProps = React.ComponentPropsWithoutRef<'div'>
+type FileProps = {
+  name: string
+  icon?: React.ReactNode
+} & React.ComponentPropsWithoutRef<'div'>
+type FolderProps = {
+  name: string
+  defaultOpen?: boolean
+} & React.ComponentPropsWithoutRef<'div'>
+
+const item = cva(
+  'flex flex-row items-center gap-2 rounded-md px-2 py-1.5 text-sm hover:bg-accent hover:text-accent-foreground'
+)
+
+export const Files = (props: FilesProps) => {
+  const { children, className, ...rest } = props
+
+  return (
+    <div
+      className={cn('not-prose rounded-md border bg-card p-2', className)}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
+
+export const File = (props: FileProps) => {
+  const { name, className, ...rest } = props
+
+  return (
+    <div className={cn(item({ className }))} {...rest}>
+      <FileIcon className='size-4' />
+      {name}
+    </div>
+  )
+}
+
+export const Folder = (props: FolderProps) => {
+  const { children, name, defaultOpen = false, ...rest } = props
+  const [open, setOpen] = React.useState(defaultOpen)
+
+  return (
+    <Collapsible open={open} onOpenChange={setOpen} {...rest}>
+      <CollapsibleTrigger className={cn(item({ className: 'w-full' }))}>
+        {open ? (
+          <FolderOpenIcon className='size-4' />
+        ) : (
+          <FolderIcon className='size-4' />
+        )}
+        {name}
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <div className='ml-2 flex flex-col border-l pl-2'>{children}</div>
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
