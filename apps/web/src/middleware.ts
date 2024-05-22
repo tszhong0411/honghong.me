759f527819e98ffe07c7c9218008bfccf36bf5a2
tszhong0411 <me@honghong.me>
2024-05-23 00:56:51 +0800

    feat: comment section

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 const middleware = () => {
   const csp = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' giscus.app *.honghong.me vercel.live;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' *.honghong.me vercel.live;
     style-src 'self' 'unsafe-inline' vercel.live;
     img-src * blob: data:;
     font-src 'self' assets.vercel.com fonts.gstatic.com vercel.live;
@@ -13,7 +13,7 @@ const middleware = () => {
     connect-src *;
     media-src 'self';
     frame-ancestors 'none';
-    frame-src giscus.app vercel.live;
+    frame-src vercel.live;
     block-all-mixed-content;
     upgrade-insecure-requests;
     worker-src blob:;
