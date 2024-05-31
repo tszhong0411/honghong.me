8b4ea81bf7526861fc2caee8ec5f07ea7eb4622d
tszhong0411 <me@honghong.me>
2024-05-31 17:56:33 +0800

    chore: remove comment

--- packages/mdx/tsup.config.ts
+++ packages/mdx/tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  // entry: ['src/index.ts', 'src/cli/index.ts', 'src/core/client.tsx'],
   entry: {
     index: 'src/index.ts',
     cli: 'src/cli/index.ts',
