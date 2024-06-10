e56a96595ccc1d702377c74d3329d77f247c22ca
tszhong0411 <me@honghong.me>
2024-06-10 09:01:37 +0800

    style: sort json

---
+++ .changeset/cold-apples-roll.md
@@ -0,0 +1,9 @@
+---
+"@tszhong0411/tailwind-config": patch
+"@tszhong0411/eslint-config": patch
+"@tszhong0411/tsconfig": patch
+"@tszhong0411/utils": patch
+"@tszhong0411/ui": patch
+---
+
+sort json
--- packages/eslint-config/src/configs/jsonc.ts
+++ packages/eslint-config/src/configs/jsonc.ts
@@ -3,5 +3,10 @@ import type { FlatESLintConfig } from 'eslint-define-config'
 import { jsoncPlugin } from '../plugins'
 
 export const jsonc: FlatESLintConfig[] = [
-  ...jsoncPlugin.configs['flat/recommended-with-json']
+  ...jsoncPlugin.configs['flat/recommended-with-json'],
+  {
+    rules: {
+      'jsonc/sort-keys': 'error'
+    }
+  }
 ]
