7502882d7d4b8dc65851dce824d269f1089e39cc
tszhong0411 <me@honghong.me>
2024-06-13 14:49:55 +0800

    chore(root): update commitlint config

---
+++ .commitlintrc.js
@@ -0,0 +1,20 @@
+import fs from 'node:fs/promises'
+
+const getDirectories = async (source) => {
+  const directories = await fs.readdir(source, { withFileTypes: true })
+
+  return directories.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
+}
+
+const apps = await getDirectories(`${import.meta.dirname}/apps`)
+const packages = await getDirectories(`${import.meta.dirname}/packages`)
+
+const scopes = [...apps, ...packages]
+
+/** @type {import('cz-git').UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', ['root', ...scopes]]
+  }
+}
