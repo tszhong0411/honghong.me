48f3ccb652949fe107a461c7b8f0d4e6c882a3e8
tszhong0411 <me@honghong.me>
2024-06-13 13:31:24 +0800

    refactor(root): use node:fs/promises

--- scripts/fix-cspell.ts
+++ scripts/fix-cspell.ts
@@ -1,37 +1,37 @@
 /**
  * Adapted from: https://github.com/mermaid-js/mermaid/blob/7bcba294930aadeaa3960865c26a06ebc7ff907a/scripts/fixCSpell.ts
  */
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 const cspellDirectory = './.cspell'
 
-const sortWordsInFile = (path: string) => {
-  const words = fs
-    .readFileSync(path, 'utf8')
+const sortWordsInFile = async (path: string) => {
+  const words = await fs.readFile(path, 'utf8')
+  const sortedWords = words
     .split('\n')
     .map((word) => word.trim().toLowerCase())
     .filter((word) => word.length > 0)
+    .sort((a, b) => a.localeCompare(b))
 
-  words.sort((a, b) => a.localeCompare(b))
-
-  fs.writeFileSync(path, words.join('\n') + '\n', 'utf8')
+  await fs.writeFile(path, sortedWords.join('\n') + '\n', 'utf8')
 }
 
-const findDictionaries = () => {
-  const files = fs
-    .readdirSync(cspellDirectory, { withFileTypes: true })
+const findDictionaries = async () => {
+  const directory = await fs.readdir(cspellDirectory, { withFileTypes: true })
+
+  const files = directory
     .filter((dirent) => dirent.isFile())
     .filter((dirent) => dirent.name.endsWith('.txt'))
 
   return files.map((file) => `${cspellDirectory}/${file.name}`)
 }
 
-const main = () => {
-  const dictionaries = findDictionaries()
+const main = async () => {
+  const dictionaries = await findDictionaries()
 
   for (const dictionary of dictionaries) {
-    sortWordsInFile(dictionary)
+    await sortWordsInFile(dictionary)
   }
 }
 
-main()
+await main()
