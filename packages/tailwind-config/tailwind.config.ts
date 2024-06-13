b613eb37f940d1c9f9d20cd923ff0d2fa26057df
tszhong0411 <me@honghong.me>
2024-06-13 13:15:20 +0800

    style: update code style

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -65,30 +65,28 @@ const config: Partial<Config> = {
         'marquee-up': 'marquee-up var(--duration, 30s) linear infinite'
       },
       backgroundImage: {
-        'article-link':
-          'linear-gradient(rgb(0 0 0 / 0) 70%, rgb(255 15 64 / 0.4) 0)',
-        'article-link-dark':
-          'linear-gradient(rgb(0 0 0 / 0) 70%, rgb(173 32 60) 0)'
+        'article-link': 'linear-gradient(rgb(0 0 0 / 0) 70%, rgb(255 15 64 / 0.4) 0)',
+        'article-link-dark': 'linear-gradient(rgb(0 0 0 / 0) 70%, rgb(173 32 60) 0)'
       },
       typography: {
         DEFAULT: {
           css: {
-            '--tw-prose-body': 'theme(\'colors.foreground / 90%\')',
-            '--tw-prose-headings': 'theme(\'colors.foreground\')',
-            '--tw-prose-lead': 'theme(\'colors.foreground\')',
-            '--tw-prose-links': 'theme(\'colors.foreground\')',
-            '--tw-prose-bold': 'theme(\'colors.foreground\')',
-            '--tw-prose-counters': 'theme(\'colors.muted.foreground\')',
-            '--tw-prose-bullets': 'theme(\'colors.muted.foreground\')',
-            '--tw-prose-hr': 'theme(\'colors.border\')',
-            '--tw-prose-quotes': 'theme(\'colors.foreground\')',
-            '--tw-prose-quote-borders': 'theme(\'colors.border\')',
-            '--tw-prose-captions': 'theme(\'colors.foreground\')',
-            '--tw-prose-th-borders': 'theme(\'colors.border\')',
-            '--tw-prose-td-borders': 'theme(\'colors.border\')',
-            '--tw-prose-code': 'theme(\'colors.foreground\')',
-            '--tw-prose-kbd': 'theme(\'colors.foreground\')',
-            '--tw-prose-kbd-shadows': 'theme(\'colors.primary.DEFAULT / 50%\')',
+            '--tw-prose-body': "theme('colors.foreground / 90%')",
+            '--tw-prose-headings': "theme('colors.foreground')",
+            '--tw-prose-lead': "theme('colors.foreground')",
+            '--tw-prose-links': "theme('colors.foreground')",
+            '--tw-prose-bold': "theme('colors.foreground')",
+            '--tw-prose-counters': "theme('colors.muted.foreground')",
+            '--tw-prose-bullets': "theme('colors.muted.foreground')",
+            '--tw-prose-hr': "theme('colors.border')",
+            '--tw-prose-quotes': "theme('colors.foreground')",
+            '--tw-prose-quote-borders': "theme('colors.border')",
+            '--tw-prose-captions': "theme('colors.foreground')",
+            '--tw-prose-th-borders': "theme('colors.border')",
+            '--tw-prose-td-borders': "theme('colors.border')",
+            '--tw-prose-code': "theme('colors.foreground')",
+            '--tw-prose-kbd': "theme('colors.foreground')",
+            '--tw-prose-kbd-shadows': "theme('colors.primary.DEFAULT / 50%')",
             '--tw-prose-pre-bg': false,
             '--tw-prose-pre-code': false,
 
@@ -99,15 +97,14 @@ const config: Partial<Config> = {
             },
 
             kbd: {
-              boxShadow:
-                '0 0 0 1px var(--tw-prose-kbd-shadows),0 3px 0 var(--tw-prose-kbd-shadows)'
+              boxShadow: '0 0 0 1px var(--tw-prose-kbd-shadows),0 3px 0 var(--tw-prose-kbd-shadows)'
             },
 
             code: {
               padding: '2px 4px',
               fontSize: '13px',
               borderRadius: '6px',
-              background: 'theme(\'colors.secondary.DEFAULT / 50%\')',
+              background: "theme('colors.secondary.DEFAULT / 50%')",
               border: '1px solid hsl(var(--border))'
             },
 
