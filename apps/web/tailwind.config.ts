a5aed7e33092362c2bff849ec29f77ac5e35cf5b
tszhong0411 <me@honghong.me>
2024-06-20 19:34:41 +0800

    refactor(web): use ui plugin

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 import sharedConfig from '@tszhong0411/tailwind-config'
 import { type Config } from 'tailwindcss'
 
-const config: Pick<Config, 'presets' | 'content' | 'darkMode' | 'theme'> = {
+const config: Config = {
   darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx,md,mdx}', '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'],
   presets: [sharedConfig],
