32f7061859adfb350d578faee083b2a30953da5f
tszhong0411 <me@honghong.me>
2024-06-20 17:50:58 +0800

    feat(prettier-config): update prettier config

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,28 +1,42 @@
 import { type Config } from 'prettier'
+import { type PluginOptions } from 'prettier-plugin-tailwindcss'
 
 type SortJsonOptions = {
   jsonRecursiveSort?: boolean
   jsonSortOrder?: string
 }
 
-const tszhong0411 = (): Config & SortJsonOptions => ({
-  arrowParens: 'always',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 2,
-  semi: false,
-  trailingComma: 'none',
-  endOfLine: 'lf',
-  plugins: [
-    'prettier-plugin-prisma',
-    'prettier-plugin-sort-json',
-    'prettier-plugin-package-perfection',
-    'prettier-plugin-tailwindcss'
-  ],
-  printWidth: 100,
+type Options = Config & SortJsonOptions & PluginOptions
 
-  // Sort JSON
-  jsonRecursiveSort: true
-})
+const tszhong0411 = (options: Options = {}): Options => {
+  const { plugins = [], ...rest } = options
+
+  return {
+    arrowParens: 'always',
+    singleQuote: true,
+    jsxSingleQuote: true,
+    tabWidth: 2,
+    semi: false,
+    trailingComma: 'none',
+    endOfLine: 'lf',
+    plugins: [
+      'prettier-plugin-prisma',
+      'prettier-plugin-sort-json',
+      'prettier-plugin-package-perfection',
+      ...plugins,
+
+      'prettier-plugin-tailwindcss' // must be loaded last
+    ],
+    printWidth: 100,
+
+    // Sort JSON
+    jsonRecursiveSort: true,
+
+    // Tailwind CSS
+    tailwindFunctions: ['cn', 'clsx', 'cva'],
+
+    ...rest
+  }
+}
 
 export default tszhong0411
