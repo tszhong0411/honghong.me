b28f3121eaa01acc00264ea1bca238e32eadedeb
tszhong0411 <me@honghong.me>
2024-07-24 02:41:49 +0800

    test(web): add theme test

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,10 +1,23 @@
-import tszhong0411 from '@tszhong0411/eslint-config'
+import tszhong0411, { GLOB_E2E } from '@tszhong0411/eslint-config'
 
-export default tszhong0411({
-  project: './tsconfig.json',
-  tsconfigRootDir: import.meta.dirname,
-  react: true,
-  next: true,
-  playwright: true,
-  testingLibrary: true
-})
+export default tszhong0411(
+  {
+    project: './tsconfig.json',
+    tsconfigRootDir: import.meta.dirname,
+    react: true,
+    next: true,
+    playwright: true,
+    testingLibrary: true
+  },
+  {
+    files: [GLOB_E2E],
+    rules: {
+      'playwright/expect-expect': [
+        'error',
+        {
+          assertFunctionNames: ['checkStoredTheme', 'checkAppliedTheme']
+        }
+      ]
+    }
+  }
+)
---
+++ apps/web/src/e2e/utils/theme.ts
@@ -0,0 +1,39 @@
+import { type Browser, expect, type Page } from '@playwright/test'
+
+export const checkAppliedTheme = async (page: Page, theme: string) => {
+  expect(await page.evaluate((t) => document.documentElement.classList.contains(t), theme)).toBe(
+    true
+  )
+  expect(await page.evaluate(() => document.documentElement.getAttribute('style'))).toBe(
+    `color-scheme: ${theme};`
+  )
+}
+
+export const checkStoredTheme = async (page: Page, expectedTheme: string) => {
+  const localStorage = await page.evaluate(() => window.localStorage)
+  expect(localStorage.theme).toBe(expectedTheme)
+}
+
+type CreateBrowserContextOptions = {
+  baseURL?: string
+  colorScheme?: 'light' | 'dark' | 'no-preference'
+  localStorage?: Array<{ name: string; value: string }>
+}
+
+export const createBrowserContext = async (
+  browser: Browser,
+  options: CreateBrowserContextOptions
+) => {
+  return await browser.newContext({
+    colorScheme: options.colorScheme ?? 'no-preference',
+    storageState: {
+      cookies: [],
+      origins: [
+        {
+          origin: options.baseURL ?? 'http://localhost:3000',
+          localStorage: options.localStorage ?? []
+        }
+      ]
+    }
+  })
+}
