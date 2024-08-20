1c0ed444bd85687e6ebbf9e98de65b9e2671e67d
tszhong0411 <me@honghong.me>
2024-08-21 04:14:55 +0800

    chore(root): tree-shakable

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.ts'],
   dts: true,
   format: ['esm'],
-  target: 'esnext'
+  target: 'esnext',
+  clean: true
 })
