3eaf968c56117968d0af4ffde4088d496caf587a
tszhong0411 <me@honghong.me>
2024-08-24 16:58:28 +0800

    feat(docs): update textarea demos

--- apps/docs/src/components/demos/textarea/disabled.tsx
+++ apps/docs/src/components/demos/textarea/disabled.tsx
@@ -1,7 +1,7 @@
 import { Textarea } from '@tszhong0411/ui'
 
 const TextareaDisabled = () => {
-  return <Textarea placeholder='Type your message here.' disabled />
+  return <Textarea placeholder='Type your message here.' className='max-w-sm' disabled />
 }
 
 export default TextareaDisabled
--- apps/docs/src/components/demos/textarea/textarea.tsx
+++ apps/docs/src/components/demos/textarea/textarea.tsx
@@ -1,7 +1,7 @@
 import { Textarea } from '@tszhong0411/ui'
 
 const TextareaDemo = () => {
-  return <Textarea placeholder='Type your message here.' />
+  return <Textarea placeholder='Type your message here.' className='max-w-sm' />
 }
 
 export default TextareaDemo
