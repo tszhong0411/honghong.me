6f393a7bde945ce37c31b32d5ffb673d86ed5210
tszhong0411 <me@honghong.me>
2024-12-12 00:04:23 +0800

    fix(docs): value of input in sheet demo should be defaultValue

--- apps/docs/src/components/demos/sheet/sheet.tsx
+++ apps/docs/src/components/demos/sheet/sheet.tsx
@@ -32,13 +32,13 @@ const SheetDemo = () => {
             <Label htmlFor='name' className='text-right'>
               Name
             </Label>
-            <Input id='name' value='Pedro Duarte' className='col-span-3' />
+            <Input id='name' defaultValue='Pedro Duarte' className='col-span-3' />
           </div>
           <div className='grid grid-cols-4 items-center gap-4'>
             <Label htmlFor='username' className='text-right'>
               Username
             </Label>
-            <Input id='username' value='@peduarte' className='col-span-3' />
+            <Input id='username' defaultValue='@peduarte' className='col-span-3' />
           </div>
         </div>
         <SheetFooter>
