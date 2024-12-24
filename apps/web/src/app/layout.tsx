f4f6bafba3814f21f8ba3efe5df00a4ef9aa511f
tszhong0411 <me@honghong.me>
2024-12-24 20:10:17 +0800

    feat(web): upgrade dependencies

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { GeistSans } from 'geist/font/sans'
 import type { Metadata, Viewport } from 'next'
 import localFont from 'next/font/local'
 import Image from 'next/image'
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 import '@/styles/globals.css'
 import Analytics from '@/components/analytics'
@@ -125,35 +126,37 @@ const Layout = (props: LayoutProps) => {
       suppressHydrationWarning
     >
       <body className='relative'>
-        <Providers>
-          <Hello />
-          <Header />
-          <main id='skip-nav' className='mx-auto mb-16 max-w-5xl px-5 py-24 sm:px-8'>
-            {children}
-          </main>
+        <NuqsAdapter>
+          <Providers>
+            <Hello />
+            <Header />
+            <main id='skip-nav' className='mx-auto mb-16 max-w-5xl px-5 py-24 sm:px-8'>
+              {children}
+            </main>
 
-          <Footer />
-          {flags.analytics ? <Analytics /> : null}
-          <SignInModal />
-          <Image
-            width={1512}
-            height={550}
-            className='absolute left-1/2 top-0 -z-10 -translate-x-1/2'
-            src='/images/gradient-background-top.png'
-            alt=''
-            role='presentation'
-            priority
-          />
-          <Image
-            width={1512}
-            height={447}
-            className='absolute -bottom-6 left-1/2 -z-10 -translate-x-1/2'
-            src='/images/gradient-background-bottom.png'
-            alt=''
-            role='presentation'
-            priority
-          />
-        </Providers>
+            <Footer />
+            {flags.analytics ? <Analytics /> : null}
+            <SignInModal />
+            <Image
+              width={1512}
+              height={550}
+              className='absolute left-1/2 top-0 -z-10 -translate-x-1/2'
+              src='/images/gradient-background-top.png'
+              alt=''
+              role='presentation'
+              priority
+            />
+            <Image
+              width={1512}
+              height={447}
+              className='absolute -bottom-6 left-1/2 -z-10 -translate-x-1/2'
+              src='/images/gradient-background-bottom.png'
+              alt=''
+              role='presentation'
+              priority
+            />
+          </Providers>
+        </NuqsAdapter>
         <SpeedInsights />
       </body>
     </html>
