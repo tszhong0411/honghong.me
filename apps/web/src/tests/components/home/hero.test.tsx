62fbf40c43d4394b0dee05a55c2c99e5eda016b6
tszhong0411 <me@honghong.me>
2025-01-03 23:50:54 +0800

    test(web): fix a failed test

--- apps/web/src/tests/components/home/hero.test.tsx
+++ apps/web/src/tests/components/home/hero.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/react'
+import { NextIntlClientProvider } from '@tszhong0411/i18n/client'
+import messages from '@tszhong0411/i18n/messages/en.json'
 import { describe, expect, it } from 'vitest'
 
 import Hero from '@/components/home/hero'
 
 describe('<Hero />', () => {
   it('should have a hero image', () => {
-    render(<Hero />)
+    render(
+      <NextIntlClientProvider locale='en' messages={messages}>
+        <Hero />
+      </NextIntlClientProvider>
+    )
 
     expect(screen.getByAltText('Hong')).toBeInTheDocument()
   })
