a9952ba03978b64eecc471435d9f210127f77296
tszhong0411 <me@honghong.me>
2025-01-06 03:18:38 +0800

    feat(mdx): restructure TOC handling and update plugin architecture

--- packages/mdx/src/core/types.ts
+++
@@ -1,9 +0,0 @@
-import { type MDXProvider } from '@mdx-js/react'
-
-export type MDXComponents = React.ComponentProps<typeof MDXProvider>['components']
-
-export type TOC = {
-  title: string
-  url: string
-  depth: number
-}
