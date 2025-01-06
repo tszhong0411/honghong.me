897a931a7083e50e50288671eaee9f742fd41859
tszhong0411 <me@honghong.me>
2025-01-07 00:17:07 +0800

    refactor(web): simplify header rendering in users table component

--- apps/web/src/components/admin/users/users-table.tsx
+++ apps/web/src/components/admin/users/users-table.tsx
@@ -24,15 +24,13 @@ const UsersTable = (props: UsersTableProps) => {
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                )
-              })}
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableHead>
+              ))}
             </TableRow>
           ))}
         </TableHeader>
