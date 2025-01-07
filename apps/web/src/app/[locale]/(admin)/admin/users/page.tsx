ee18b6faeb43b3afb69eec480a50c37291987fbe
tszhong0411 <me@honghong.me>
2025-01-08 00:14:22 +0800

    feat(web): users table with filtering and loading states

--- apps/web/src/app/[locale]/(admin)/admin/users/page.tsx
+++ apps/web/src/app/[locale]/(admin)/admin/users/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 
-import UsersTable from '@/components/admin/users/users-table'
+import { useTranslations } from '@tszhong0411/i18n/client'
+import { DataTableSkeleton } from '@tszhong0411/ui'
+
+import UsersTable from '@/components/admin/users-table'
 import { api } from '@/trpc/react'
 
 const Page = () => {
   const { status, data } = api.users.getUsers.useQuery()
+  const t = useTranslations()
 
   const isLoading = status === 'pending'
   const isError = status === 'error'
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return (
+      <DataTableSkeleton columnCount={3} searchableColumnsCount={1} filterableColumnCount={1} />
+    )
   }
 
   if (isError) {
-    return <div>Error</div>
+    return <div>{t('admin.table.users.failed-to-fetch-users')}</div>
   }
 
   return <UsersTable data={data.users} />
