197d12270ff1a3333113f2eff951193dec3f71e0
tszhong0411 <me@honghong.me>
2025-01-09 18:30:47 +0800

    feat(ui): add switch component

---
+++ packages/ui/src/switch.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import * as SwitchPrimitives from '@radix-ui/react-switch'
+import { cn } from '@tszhong0411/utils'
+
+type SwitchProps = React.ComponentProps<typeof SwitchPrimitives.Root>
+
+export const Switch = (props: SwitchProps) => {
+  const { className, ...rest } = props
+
+  return (
+    <SwitchPrimitives.Root
+      className={cn(
+        'focus-visible:ring-ring focus-visible:ring-offset-background data-[state=checked]:bg-primary data-[state=unchecked]:bg-input peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        className
+      )}
+      {...rest}
+    >
+      <SwitchPrimitives.Thumb
+        className={cn(
+          'bg-background pointer-events-none block size-4 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0'
+        )}
+      />
+    </SwitchPrimitives.Root>
+  )
+}
