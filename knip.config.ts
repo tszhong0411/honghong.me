1d94e06101f4fb015765d751c5cda156b09d3a7d
tszhong0411 <me@honghong.me>
2025-01-09 23:26:12 +0800

    chore: update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -34,6 +34,10 @@ const config: KnipConfig = {
     'packages/emails': {
       // Required for React 19
       ignoreDependencies: ['react-dom', '@types/react-dom']
+    },
+    'packages/ui': {
+      // https://github.com/shadcn-ui/ui/issues/4792
+      ignoreDependencies: ['@radix-ui/react-context']
     }
   }
 }
