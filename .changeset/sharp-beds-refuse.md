cc65095c472b39ba9a460706f47c5e9db4b0c4d9
tszhong0411 <me@honghong.me>
2025-01-12 01:04:44 +0800

    fix(ui): update dialog and alert dialog styles for consistent rounding

---
+++ .changeset/sharp-beds-refuse.md
@@ -0,0 +1,5 @@
+---
+'@tszhong0411/ui': patch
+---
+
+Show rounding on mobile as well for alert dialog and dialog
--- packages/ui/src/alert-dialog.tsx
+++ packages/ui/src/alert-dialog.tsx
@@ -37,7 +37,7 @@ export const AlertDialogContent = (props: AlertDialogContentProps) => {
       <AlertDialogOverlay />
       <AlertDialogPrimitive.Content
         className={cn(
-          'bg-background fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 border p-6 shadow-lg sm:rounded-lg',
+          'bg-background fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg border p-6 shadow-lg',
           'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
           'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]',
           className
