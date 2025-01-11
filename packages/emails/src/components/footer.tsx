49233046cb03759f3df64721e8d011b9e74701d8
tszhong0411 <me@honghong.me>
2025-01-11 13:13:51 +0800

    feat(emails): redesign comment and reply templates

---
+++ packages/emails/src/components/footer.tsx
@@ -0,0 +1,33 @@
+import { Column, Hr, Img, Link, Row, Section, Text } from '@react-email/components'
+
+const Footer = () => {
+  return (
+    <>
+      <Hr className='border-t-border mb-3 mt-6' />
+      <Section>
+        <Row className='mt-4' align='left' width='auto'>
+          <Column className='pr-6 align-middle'>
+            <Link href='https://twitter.com/tszhong0411' className='text-xl text-black'>
+              <Img src='https://honghong.me/images/email/x.png' alt='X' width={22} height={22} />
+            </Link>
+          </Column>
+          <Column className='align-middle'>
+            <Link href='https://github.com/tszhong0411/honghong.me' className='text-xl text-black'>
+              <Img
+                src='https://honghong.me/images/email/github.png'
+                alt='GitHub'
+                width={22}
+                height={22}
+              />
+            </Link>
+          </Column>
+        </Row>
+      </Section>
+      <Text className='m-0 mt-6 p-0 text-xs font-normal text-gray-500'>
+        © {new Date().getFullYear()} Hong. All rights reserved.
+      </Text>
+    </>
+  )
+}
+
+export default Footer
