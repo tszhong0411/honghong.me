03220a75520ace2f54639b8f09adb3ff97b38f90
tszhong0411 <me@honghong.me>
2025-01-15 02:42:24 +0800

    refactor(ui): update badge styles

---
+++ .changeset/nine-walls-design.md
@@ -0,0 +1,5 @@
+---
+'@tszhong0411/ui': patch
+---
+
+Badge - remove focus and hover state styles, use rounded-full, add shadow to secondary variant
--- packages/ui/src/badge.tsx
+++ packages/ui/src/badge.tsx
@@ -2,15 +2,13 @@ import { cn } from '@tszhong0411/utils'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export const badgeVariants = cva(
-  'focus:ring-ring inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
+  'inline-flex items-center justify-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent shadow',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent shadow',
+        default: 'bg-primary text-primary-foreground border-transparent shadow',
+        secondary: 'bg-secondary text-secondary-foreground border-transparent shadow',
+        destructive: 'bg-destructive text-destructive-foreground border-transparent shadow',
         outline: 'text-foreground'
       }
     },
