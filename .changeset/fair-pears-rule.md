49be385fde0945345d588f692b2730bd85b3ff83
tszhong0411 <me@honghong.me>
2025-01-16 01:18:37 +0800

    fix(ui): move the CheckIcon to the left in Select component

---
+++ .changeset/fair-pears-rule.md
@@ -0,0 +1,5 @@
+---
+'@tszhong0411/ui': patch
+---
+
+Move the CheckIcon to the left in Select component
--- packages/ui/src/select.tsx
+++ packages/ui/src/select.tsx
@@ -123,14 +123,14 @@ const SelectItem = (props: SelectItemProps) => {
   return (
     <SelectPrimitive.Item
       className={cn(
-        'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none',
+        'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors',
         'focus:bg-accent focus:text-accent-foreground',
         'data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
         className
       )}
       {...rest}
     >
-      <span className='absolute right-2 flex size-3.5 items-center justify-center'>
+      <span className='absolute left-2 flex size-3.5 items-center justify-center'>
         <SelectPrimitive.ItemIndicator>
           <CheckIcon className='size-4' />
         </SelectPrimitive.ItemIndicator>
