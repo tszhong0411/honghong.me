663c5df918464f146627e0d2ef88f6c1d37b7e1d
tszhong0411 <me@honghong.me>
2025-01-16 07:28:23 +0800

    refactor(ui): update focus-visible ring styles in ResizableHandle for improved accessibility

--- packages/ui/src/resizable.tsx
+++ packages/ui/src/resizable.tsx
@@ -31,7 +31,7 @@ const ResizableHandle = (props: ResizableHandleProps) => {
       className={cn(
         'bg-border relative flex w-px items-center justify-center',
         'data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0',
-        'focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1',
+        'focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
         'after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2',
         '[&[data-panel-group-direction=vertical]>div]:rotate-90',
         className
