546804832b92df8398c8ad1cfe3d8a383d116270
tszhong0411 <me@honghong.me>
2025-01-18 16:03:05 +0800

    refactor(i18n): set request in i18n package directly

--- packages/i18n/src/plugin.ts
+++
@@ -1,6 +0,0 @@
-import type { NextConfig } from 'next'
-import withNextIntl from 'next-intl/plugin'
-
-export const withI18n = (requestConfigPath: string, config: NextConfig) => {
-  return withNextIntl(requestConfigPath)(config)
-}
---
+++ packages/i18n/src/request.ts
@@ -0,0 +1,19 @@
+import { getRequestConfig } from 'next-intl/server'
+
+import { i18n } from './config'
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale
+
+  if (!locale || !i18n.locales.includes(locale)) {
+    locale = i18n.defaultLocale
+  }
+
+  return {
+    messages: {
+      ...(await import(`./messages/${locale}.json`)).default
+    },
+    locale,
+    timeZone: 'UTC'
+  }
+})
