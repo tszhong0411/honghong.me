e931213208763f6fb6b8d4ac0b8aba138719d860
tszhong0411 <me@honghong.me>
2025-01-26 15:00:39 +0800

    refactor: migrate configuration to TypeScript and remove old file

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,8 @@
 import fs from 'node:fs/promises'
 
-const getDirectories = async (source) => {
+import { type UserConfig } from 'cz-git'
+
+const getDirectories = async (source: string) => {
   const directories = await fs.readdir(source, { withFileTypes: true })
 
   return directories.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
@@ -11,10 +13,11 @@ const packages = await getDirectories(`${import.meta.dirname}/packages`)
 
 const scopes = [...apps, ...packages]
 
-/** @type {import('cz-git').UserConfig} */
-export default {
+const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', ['root', 'release', ...scopes]]
   }
 }
+
+export default config
