5f8b69548671c18eb1442f6ec61b8ae3e64179dc
tszhong0411 <me@honghong.me>
2025-02-01 00:46:18 +0800

    chore(ui): update paths for CSS files and include CSS in build process

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*@(ts|tsx)'],
+  entry: ['src/**/*@(ts|tsx)', 'src/styles/*.css'],
   dts: true,
   format: ['esm'],
   target: 'esnext',
-  clean: true
+  clean: true,
+  loader: {
+    // Just copy the CSS files
+    '.css': 'copy'
+  }
 })
