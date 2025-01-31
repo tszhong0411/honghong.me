27478c912bc98f71574355657566f6282ae2a7bd
tszhong0411 <me@honghong.me>
2025-02-01 03:40:46 +0800

    fix: add `@tailwindcss/typography` to ignored dependencies in Knip config

--- knip.config.ts
+++ knip.config.ts
@@ -35,7 +35,7 @@ const config: KnipConfig = {
     },
     'packages/ui': {
       // https://github.com/shadcn-ui/ui/issues/4792
-      ignoreDependencies: ['@radix-ui/react-context']
+      ignoreDependencies: ['@radix-ui/react-context', '@tailwindcss/typography']
     }
   }
 }
