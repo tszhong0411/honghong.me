0211d2536f65282df0f1b601e3f367707db6b969
tszhong0411 <me@honghong.me>
2025-02-03 00:26:06 +0800

    feat: update console log message

--- apps/web/src/components/hello.ts
+++ apps/web/src/components/hello.ts
@@ -4,14 +4,18 @@ import { useEffect } from 'react'
 
 const Hello = () => {
   useEffect(() => {
-    console.log(`
-██╗  ██╗ ██████╗ ███╗   ██╗ ██████╗
-██║  ██║██╔═══██╗████╗  ██║██╔════╝
-███████║██║   ██║██╔██╗ ██║██║  ███╗
-██╔══██║██║   ██║██║╚██╗██║██║   ██║
-██║  ██║╚██████╔╝██║ ╚████║╚██████╔╝
-╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝
-`)
+    console.log(
+      `\
+%cHey there, awesome developer!
+
+If you're digging this code, check out my GitHub repo:
+
+https://github.com/tszhong0411/honghong.me
+
+and give it a star ⭐
+`,
+      'font-size: 16px'
+    )
   }, [])
 
   return null
