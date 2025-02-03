da9325be240398928403f317ed9ef3ee75702eee
tszhong0411 <me@honghong.me>
2025-02-03 20:54:25 +0800

    fix(web): add main layout to error page

--- apps/web/src/app/[locale]/error.tsx
+++ apps/web/src/app/[locale]/error.tsx
@@ -3,6 +3,8 @@
 import { useTranslations } from '@tszhong0411/i18n/client'
 import { Button } from '@tszhong0411/ui'
 
+import MainLayout from '@/components/main-layout'
+
 type PageProps = {
   error: Error & { digest?: string }
   reset: () => void
@@ -13,11 +15,13 @@ const Page = (props: PageProps) => {
   const t = useTranslations()
 
   return (
-    <div className='h-content space-y-4 px-2 py-8'>
-      <h1 className='text-2xl font-bold'>{t('error.something-went-wrong')}</h1>
-      <Button onClick={reset}>{t('error.try-again')}</Button>
-      <p className='break-words rounded-md bg-zinc-100 p-4 dark:bg-zinc-800'>{error.message}</p>
-    </div>
+    <MainLayout>
+      <div className='h-content space-y-4 px-2 py-8'>
+        <h1 className='text-2xl font-bold'>{t('error.something-went-wrong')}</h1>
+        <Button onClick={reset}>{t('error.try-again')}</Button>
+        <p className='break-words rounded-md bg-zinc-100 p-4 dark:bg-zinc-800'>{error.message}</p>
+      </div>
+    </MainLayout>
   )
 }
 
