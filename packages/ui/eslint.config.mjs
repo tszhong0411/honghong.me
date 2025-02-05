e0d904d74dba734a925b8b2794645b6a84e1c2c4
tszhong0411 <me@honghong.me>
2025-02-06 00:47:06 +0800

    chore(ui): enable Next.js support in ESLint config as well

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -3,5 +3,6 @@ import tszhong0411 from '@tszhong0411/eslint-config'
 export default tszhong0411({
   project: './tsconfig.json',
   tsconfigRootDir: import.meta.dirname,
-  react: true
+  react: true,
+  next: true
 })
