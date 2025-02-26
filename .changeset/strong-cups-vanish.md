b932b3851d2624864ceb4bf97d36b9a0895150a4
tszhong0411 <me@honghong.me>
2025-02-26 12:00:23 +0800

    style(utils): update code style

---
+++ .changeset/strong-cups-vanish.md
@@ -0,0 +1,5 @@
+---
+'@tszhong0411/utils': patch
+---
+
+Update code style
--- packages/utils/src/cn.ts
+++ packages/utils/src/cn.ts
@@ -1,6 +1,4 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
