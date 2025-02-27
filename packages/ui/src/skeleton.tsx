abfb7cde90a0f05acae2f4a75a9d417908587ed0
tszhong0411 <me@honghong.me>
2025-02-27 17:57:54 +0800

    fix(ui): background color of skeleton

--- packages/ui/src/skeleton.tsx
+++ packages/ui/src/skeleton.tsx
@@ -5,7 +5,7 @@ type SkeletonProps = React.ComponentProps<'div'>
 const Skeleton = (props: SkeletonProps) => {
   const { className, ...rest } = props
 
-  return <div className={cn('bg-muted animate-pulse rounded-lg', className)} {...rest} />
+  return <div className={cn('bg-primary/10 animate-pulse rounded-lg', className)} {...rest} />
 }
 
 export { Skeleton }
