c68a547a19b3d0cd7f44aefa37db83931579a599
tszhong0411 <me@honghong.me>
2025-03-02 05:11:15 +0800

    test(web): correct auth setup

--- apps/web/src/e2e/auth.setup.ts
+++ apps/web/src/e2e/auth.setup.ts
@@ -2,7 +2,9 @@ import path from 'node:path'
 
 import { test as setup } from '@playwright/test'
 import { accounts, db, sessions, users } from '@tszhong0411/db'
+import { env } from '@tszhong0411/env'
 import { generateId } from 'better-auth'
+import { serializeSignedCookie } from 'better-call'
 import dayjs from 'dayjs'
 
 import { TEST_USER } from './constants'
@@ -15,60 +17,71 @@ setup('unauthenticated', async ({ page }) => {
 })
 
 setup('authenticate user', async ({ page }) => {
-  const { id: userId, token, accountId } = TEST_USER
-  const expiresAt = dayjs().add(1, 'month').toDate()
+  const expiresAt = dayjs().add(7, 'day').toDate()
 
-  await db
-    .insert(users)
-    .values({
-      id: userId,
-      name: 'Test User',
-      email: 'user@honghong.me',
-      emailVerified: true,
-      image: 'http://localhost:3000/api/avatar/test',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      role: 'user'
-    })
-    .onConflictDoNothing({ target: users.id })
+  await db.transaction(async (tx) => {
+    await tx
+      .insert(users)
+      .values({
+        id: TEST_USER.id,
+        name: TEST_USER.name,
+        email: TEST_USER.email,
+        image: TEST_USER.image,
+        emailVerified: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      .onConflictDoNothing({ target: users.id })
 
-  await db
-    .insert(sessions)
-    .values({
-      id: generateId(),
-      token,
-      userId,
-      expiresAt,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
-    .onConflictDoUpdate({ target: sessions.token, set: { expiresAt } })
+    await tx
+      .insert(accounts)
+      .values({
+        id: generateId(),
+        accountId: TEST_USER.accountId,
+        providerId: 'github',
+        userId: TEST_USER.id,
+        accessToken: 'gho_1234567890',
+        scope: 'read:user,user:email',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      .onConflictDoNothing({ target: accounts.id })
 
-  await db
-    .insert(accounts)
-    .values({
-      id: generateId(),
-      accountId,
-      providerId: 'github',
-      userId,
-      accessToken: 'gho_0000',
-      scope: 'read:user,user:email',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
-    .onConflictDoNothing({ target: [accounts.id] })
+    await tx
+      .insert(sessions)
+      .values({
+        id: generateId(),
+        token: TEST_USER.sessionToken,
+        userId: TEST_USER.id,
+        expiresAt: expiresAt,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      .onConflictDoUpdate({
+        target: sessions.token,
+        set: { expiresAt: expiresAt }
+      })
+  })
+
+  const cookie = await serializeSignedCookie(
+    'better-auth.session_token',
+    TEST_USER.sessionToken,
+    env.BETTER_AUTH_SECRET
+  )
 
   await page.context().addCookies([
     {
       name: 'better-auth.session_token',
-      value: token,
+      value: `${TEST_USER.sessionToken}.${cookie.split('.')[2]!}`,
       domain: 'localhost',
       path: '/',
       httpOnly: true,
       sameSite: 'Lax',
-      expires: Math.floor(expiresAt.getTime() / 1000)
+      expires: Math.floor(expiresAt.valueOf() / 1000)
     }
   ])
+
+  await page.goto('http://localhost:3000/')
 
   await page.context().storageState({ path: authenticatedStoragePath })
 })
--- apps/web/src/e2e/constants.ts
+++ apps/web/src/e2e/constants.ts
@@ -1,7 +1,10 @@
 export const A11Y_TAGS = ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22aa']
 
 export const TEST_USER = {
-  id: '000000000000000000000000',
-  token: '00000000000000000000000000000000',
-  accountId: '00000000'
+  id: '00000000',
+  name: 'Test User',
+  email: 'user@honghong.me',
+  image: 'http://localhost:3000/api/avatar/test',
+  accountId: '00000000',
+  sessionToken: '00000000000000000000000000000000'
 }
