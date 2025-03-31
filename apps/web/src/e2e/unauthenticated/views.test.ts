987479f08df6a11e241f7e831c1a7a17f2b08476
tszhong0411 <me@honghong.me>
2025-03-31 18:33:10 +0800

    test: reset views before and after view test

--- apps/web/src/e2e/unauthenticated/views.test.ts
+++ apps/web/src/e2e/unauthenticated/views.test.ts
@@ -1,20 +1,28 @@
 import { expect, test } from '@playwright/test'
+import { db, eq, posts } from '@tszhong0411/db'
 import { redis, redisKeys } from '@tszhong0411/kv'
 
-const removeViewsCache = async () => {
+const resetViews = async () => {
+  await db
+    .update(posts)
+    .set({
+      views: 0
+    })
+    .where(eq(posts.slug, 'test'))
+
   await redis.del(redisKeys.postViews('test'))
   await redis.del(redisKeys.postViewCount)
 }
 
 test.describe('views', () => {
   test.beforeEach(async ({ page }) => {
-    await removeViewsCache()
+    await resetViews()
     await page.goto('/blog/test')
   })
 
   test('should be able to view a post', async ({ page }) => {
     await expect(page.getByTestId('view-count')).toHaveAttribute('aria-label', '1')
 
-    await removeViewsCache()
+    await resetViews()
   })
 })
