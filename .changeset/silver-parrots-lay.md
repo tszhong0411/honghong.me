40012b99ffd755525b1c90c33f93497a2c764c20
tszhong0411 <me@honghong.me>
2025-04-12 23:46:49 +0800

    feat(ui): add background color to outline button and input

---
+++ .changeset/silver-parrots-lay.md
@@ -0,0 +1,5 @@
+---
+'@tszhong0411/ui': patch
+---
+
+Add background color to outline button and input
--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -8,7 +8,7 @@ const Input = (props: InputProps) => {
   return (
     <input
       className={cn(
-        'border-input bg-background ring-offset-background flex h-10 w-full rounded-lg border px-3 py-2',
+        'border-input bg-background dark:bg-input/30 ring-offset-background flex h-10 w-full rounded-lg border px-3 py-2',
         'file:border-0 file:bg-transparent file:text-sm file:font-medium',
         'placeholder:text-muted-foreground',
         'focus-visible:ring-ring focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2',
