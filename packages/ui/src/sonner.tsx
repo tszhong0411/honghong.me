102f7fbbf373f1aa61b761a3d0b2634527acc35f
tszhong0411 <me@honghong.me>
2025-04-19 00:10:31 +0800

    fix(ui): add back the `useTheme()` in sonner

--- packages/ui/src/sonner.tsx
+++ packages/ui/src/sonner.tsx
@@ -1,8 +1,12 @@
+import { useTheme } from 'next-themes'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
 
 const Toaster = (props: ToasterProps) => {
+  const { theme = 'system' } = useTheme()
+
   return (
     <Sonner
+      theme={theme as ToasterProps['theme']}
       className='toaster group'
       style={
         {
