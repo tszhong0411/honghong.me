e7286150c0d156c21f0b78a06991765d15f45b86
tszhong0411 <me@nelsonlai.me>
2025-06-16 15:18:22 +0800

    fix(web): broken links

--- apps/web/src/app/[locale]/(main)/blog/[slug]/footer.tsx
+++ apps/web/src/app/[locale]/(main)/blog/[slug]/footer.tsx
@@ -1,24 +1,24 @@
 'use client'
 
-import { useTranslations } from '@tszhong0411/i18n/client'
+import { useLocale, useTranslations } from '@tszhong0411/i18n/client'
 import { linkVariants } from '@tszhong0411/ui'
 
 import Link from '@/components/link'
 import { usePostContext } from '@/contexts/post'
 import { useFormattedDate } from '@/hooks/use-formatted-date'
 
-const editURL = (slug: string) =>
-  `https://github.com/tszhong0411/nelsonlai.me/blob/main/apps/web/src/content/blog/${slug}.mdx?plain=1`
-
 const Footer = () => {
   const { slug, modifiedTime } = usePostContext()
   const t = useTranslations()
+  const locale = useLocale()
+
+  const editURL = `https://github.com/tszhong0411/nelsonlai.me/blob/main/apps/web/src/content/blog/${locale}/${slug}.mdx?plain=1`
 
   const formattedDate = useFormattedDate(modifiedTime)
 
   return (
     <div className='my-8 flex w-full items-center justify-between py-4 text-sm'>
-      <Link href={editURL(slug)} className={linkVariants({ variant: 'muted' })}>
+      <Link href={editURL} className={linkVariants({ variant: 'muted' })}>
         {t('blog.footer.edit-on-github')}
       </Link>
       <div className='text-muted-foreground'>
